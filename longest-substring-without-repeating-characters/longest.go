9cce382d7f2e4e96562339f78b584734c44d3457
Shunpoco <tkngsnsk313320@gmail.com>
2021-02-25 01:57:46 +0900

    version up

--- longest-substring-without-repeating-characters/longest.go
+++ longest-substring-without-repeating-characters/longest.go
@@ -1,30 +1,24 @@
 package longest
 
 func lengthOfLongestSubstring(s string) int {
-	result := 0
+	mLen, cLen, sIdx := 0, 0, 0
 
-	temp := 0
+	magic := map[rune]int{}
 
-	l := len(s)
-
-	for i := 0; i < l; i++ {
-		hash := map[string]int{}
-		word := string(s[i])
-		temp = 1
-		hash[word]++
-		for j := i + 1; j < l; j++ {
-			word := string(s[j])
-			hash[word]++
-			if hash[word] == 1 {
-				temp++
-			}
-			if hash[word] > 1 {
-				break
-			}
+	for idx, val := range s {
+		if lIdx, ok := magic[val]; ok && lIdx >= sIdx {
+			sIdx = lIdx + 1
+			cLen = idx - lIdx
+		} else {
+			cLen++
 		}
-		if temp > result {
-			result = temp
+
+		if cLen > mLen {
+			mLen = cLen
 		}
+
+		magic[val] = idx
 	}
-	return result
+
+	return mLen
 }

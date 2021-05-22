65ecee71e4c9537758a0e78a1f9026e1c5dde862
Shunpoco <tkngsnsk313320@gmail.com>
2021-05-22 12:49:00 +0900

    Construct BT from inorder to postorder

---
+++ tree/construct-bt-inpost/README.md
@@ -0,0 +1,2 @@
+## Construct Binary Tree from Inorder and Postorder Traversal
+https://leetcode.com/explore/learn/card/data-structure-tree/133/conclusion/942/
---
+++ tree/construct-bt-inpost/python/construct.py
@@ -0,0 +1,30 @@
+from typing import List
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> TreeNode:
+        if len(inorder) == 0 or len(postorder) == 0:
+            return None
+
+        rootVal = postorder[len(postorder)-1]
+        root = TreeNode(rootVal)
+
+        inRootIdx = inorder.index(rootVal)
+
+        inLeft = inorder[:inRootIdx]
+        inRight = inorder[inRootIdx+1:]
+
+        postLeft = postorder[:len(inLeft)]
+        postRight = postorder[len(inLeft):len(postorder)-1]
+
+        root.left = self.buildTree(inLeft, postLeft)
+        root.right = self.buildTree(inRight, postRight)
+
+        return root
+
+

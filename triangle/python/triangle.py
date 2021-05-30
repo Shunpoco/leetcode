2356adfc722b8dc45474d658c389eafa76451522
Shunpoco <tkngsnsk313320@gmail.com>
2021-05-30 10:37:49 +0900

    Triangle

--- triangle/python/triangle.py
+++ triangle/python/triangle.py
@@ -3,8 +3,9 @@ from typing import List
 
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        for level in range(len(triangle)-2, -1,-1):
-            for idx in range(level+1):
+        l = len(triangle)
+        for level in range(l-2, -1, -1):
+            for idx in range(len(triangle[level])):
                 triangle[level][idx] += min(
                     triangle[level+1][idx],
                     triangle[level+1][idx+1],

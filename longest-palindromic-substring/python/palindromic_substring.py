784cd79538e335fd48506448acfce6267993f504
Shunpoco <tkngsnsk313320@gmail.com>
2021-06-09 10:08:18 +0900

    Review longest palindromic substring

--- longest-palindromic-substring/python/palindromic_substring.py
+++ longest-palindromic-substring/python/palindromic_substring.py
@@ -1,37 +1,19 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        result = s[0]
-        maxL = 1
-        l = len(s)
-        memory = {}
-        for i in range(l):
-            for j in range(l-1, i, -1):
-                res, isPalindrome = self._isPalindrome(s, i, j, memory)
-                if isPalindrome and len(res) > maxL:
-                    result = res
-                    maxL = len(res)
-        return result
+        p = ''
+
+        for i in range(len(s)):
+            p1 = self._palindrome(s, i, i+1)
+            p2 = self._palindrome(s, i, i)
+
+            p = max([p, p1, p2], key=lambda x: len(x))
 
 
-    def _isPalindrome(self, s: str, start: int, end: int, memory) -> bool:
-        sub = s[start:end+1]
-        tup = (start, end)
-        if memory.get(tup) is not None:
-            return sub, memory.get(tup)
+        return p
+    
+    def _palindrome(self, s: str, left: int, right: int) -> str:
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
 
-        if start == end:
-            memory[tup] = True
-            return sub, True
-
-        if end == start+1:
-            isPalindrome = s[start] == s[end]
-            memory[tup] = isPalindrome
-            return sub, isPalindrome
-
-        _, isPalindrome = self._isPalindrome(s, start+1, end-1, memory)
-
-        isPalindrome = isPalindrome and s[start] == s[end]
-
-        memory[tup] = isPalindrome
-
-        return sub, isPalindrome
+        return s[left+1:right]

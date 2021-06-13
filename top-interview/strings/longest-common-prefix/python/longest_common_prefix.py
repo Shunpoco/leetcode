3ef93364bdb396c3db7ffff48ca23c3512640203
Shunpoco <tkngsnsk313320@gmail.com>
2021-06-13 13:20:12 +0900

    Longest Common Prefix using Trie

--- top-interview/strings/longest-common-prefix/python/longest_common_prefix.py
+++ top-interview/strings/longest-common-prefix/python/longest_common_prefix.py
@@ -1,18 +1,60 @@
+from collections import defaultdict
+
+class TrieNode:
+    def __init__(self):
+        self.nodes = defaultdict(TrieNode)
+        self.isEnd = False
+        
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+        
+    def insert(self, word: str):
+        cur = self.root
+        
+        for char in word:
+            cur = cur.nodes[char]
+        cur.isEnd = True
+        
+    def search(self, word: str) -> bool:
+        cur = self.root
+        for char in word:
+            if char not in cur.nodes:
+                return False
+            cur = cur.nodes[char]
+        return cur.isEnd
+    
+    def startWith(self, prefix: str) -> bool:
+        cur = self.root
+        for char in prefix:
+            if char not in cur.nodes:
+                return False
+            cur = cur.nodes[char]
+        return True
+
+    def noBranch(self, prefix: str) -> bool:
+        cur = self.root
+        for char in prefix:
+            if char not in cur.nodes:
+                return False
+            if len(cur.nodes) > 1:
+                return False
+            cur = cur.nodes[char]
+        return True
+    
+
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
+        trie = Trie()
         minStr = strs[0]
-        
         for s in strs:
+            trie.insert(s)
             if len(s) < len(minStr):
                 minStr = s
                 
-        minPref = ''
         for i in range(len(minStr)):
-            for j in range(len(strs)):
-                s = strs[j]
-                if minStr[i] != s[i]:
-                    return minPref
-                if j == len(strs)-1:
-                    minPref += minStr[i]
-                    
-        return minPref
+            s = minStr[0:len(minStr)-i]
+            if trie.noBranch(s):
+                return s
+            
+        return ''

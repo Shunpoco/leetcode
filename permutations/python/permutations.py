328b06de8cd69cbe4a17b266138ff9e5bd50e143
Shunpoco <tkngsnsk313320@gmail.com>
2021-06-22 22:52:08 +0900

    Permutations

---
+++ permutations/python/permutations.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        stack = [([], 0)]
+        result = []
+        L = len(nums)
+        while stack:
+            perms, level = stack.pop(-1)
+            for num in nums:
+                if num not in perms:
+                    temp = perms.copy()
+                    temp.append(num)
+                    if level+1 == L:
+                        result.append(temp)
+                    else:
+                        stack.append((temp, level+1))
+                        
+        return result

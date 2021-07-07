482ab06a082b3246847eaa97d8efb19b8dbaf08b
Shunpoco <tkngsnsk313320@gmail.com>
2021-07-07 22:38:10 +0900

    max-depth using DFS

---
+++ tree/max-depth/README.md
@@ -0,0 +1,2 @@
+## Maximum Depth of Binary Tree
+https://leetcode.com/explore/learn/card/data-structure-tree/17/solve-problems-recursively/535/
--- tree/max-depth/python/max-depth.py
+++ tree/max-depth/python/max-depth.py
@@ -4,6 +4,7 @@ class TreeNode:
         self.left = left
         self.right = right
 
+# Using recursion
 class Solution:
     def maxDepth(self, root: TreeNode) -> int:
         return self._count(root, 0)
@@ -16,3 +17,30 @@ class Solution:
 
         return max(self._count(root.left, count), self._count(root.right, count))
 
+
+# Using stack (Depth first search)
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        md = 0
+        if root is None:
+            return md
+        
+        stack = []
+        
+        stack.append((root, 1))
+        
+        while stack:
+            node, level = stack.pop(-1)
+            
+            if node is None:
+                continue
+            
+            if node.left is None and node.right is None:
+                if level > md:
+                    md = level
+                continue
+                
+            stack.append((node.left, level+1))
+            stack.append((node.right, level+1))
+            
+        return md

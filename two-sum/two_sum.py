6176efaad418df1d71436f2b71b0b2bb01896a02
Shunpoco <tkngsnsk313320@gmail.com>
2021-07-10 22:25:39 +0900

    Two sum

--- two-sum/two_sum.py
+++ two-sum/two_sum.py
@@ -2,13 +2,16 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        ht = {}
-
-        for i in range(len(nums)):
-            ht[nums[i]] = i
-
-        for i in range(len(nums)):
-            v = nums[i]
-            j = ht.get(target-v)
-            if j is not None and i != j:
-                return [i, j]
+        h = {}
+        
+        for idx in range(len(nums)):
+            v = nums[idx]
+            h[target-v] = idx
+            
+        for idx in range(len(nums)):
+            v = nums[idx]
+            
+            if h.get(v) and h[v] != idx:
+                return [idx, h[v]]
+                        
+        return []

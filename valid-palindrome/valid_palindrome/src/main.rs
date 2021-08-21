499cc792ac7e2ea1212f96eaa92982dff7665c78
Shunpoco <tkngsnsk313320@gmail.com>
2021-08-21 11:34:16 +0900

    valid palindrome on Rust

---
+++ valid-palindrome/valid_palindrome/src/main.rs
@@ -0,0 +1,46 @@
+struct Solution {}
+
+impl Solution {
+    pub fn is_palindrome(s: String) -> bool {
+        let sc: Vec<char> = s.chars().collect();
+        let mut left = 0usize;
+        let mut right = (sc.len() - 1) as usize;
+        
+        loop {
+            while !sc[left].is_alphanumeric() && left < right {
+                left += 1;
+            }
+            
+            while !sc[right].is_alphanumeric() && right > left {
+                right -= 1;
+            }
+            
+            if left >= right {
+                break;
+            }
+            
+            // compare
+            let l = sc[left].to_lowercase().to_string();
+            let r = sc[right].to_lowercase().to_string();
+            if l != r {
+                return false;
+            }
+            
+            left += 1;
+            right -= 1;
+        }
+        
+        true
+    }
+}
+
+fn main() {
+    assert!(Solution::is_palindrome("A man, a plan, a canal: Panama".to_string()));
+}
+
+#[test]
+fn test_palindrome() {
+    assert_eq!(Solution::is_palindrome("A man, a plan, a canal: Panama".to_string()), true);
+    assert_eq!(Solution::is_palindrome(" ".to_string()), true);
+    assert_eq!(Solution::is_palindrome("race a car".to_string()), false);
+}

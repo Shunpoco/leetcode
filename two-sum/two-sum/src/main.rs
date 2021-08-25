ae16f3a8896cdcc3494e9cf4a704f5dc35e1473f
Shunpoco <tkngsnsk313320@gmail.com>
2021-08-25 22:21:01 +0900

    two sum on Rust 2

--- two-sum/two-sum/src/main.rs
+++ two-sum/two-sum/src/main.rs
@@ -2,20 +2,39 @@ use std::collections::HashMap;
 
 struct Solution{}
 
+// impl Solution {
+//     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+//         let mut h = HashMap::new();
+        
+//         for i in 0..nums.len() {
+//             let v = nums[i];
+//             if h.contains_key(&v) {
+//                 return vec![h[&v] as i32, i as i32];
+//             }
+//             h.insert(target-nums[i], i);
+//         }
+//         vec![]
+//     }
+// }
+
+// 2021-08-25
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let mut h = HashMap::new();
+        let mut h: HashMap<i32, i32> = HashMap::new();
         
         for i in 0..nums.len() {
-            let v = nums[i];
-            if h.contains_key(&v) {
-                return vec![h[&v] as i32, i as i32];
+            match h.get(&nums[i]) {
+                None => {
+                    h.insert(target - nums[i], i as i32);
+                },
+                Some(v) => return vec![*v, i as i32]
             }
-            h.insert(target-nums[i], i);
         }
+        
         vec![]
     }
 }
+
 
 fn main() {
     Solution::two_sum(vec![3,3], 6);

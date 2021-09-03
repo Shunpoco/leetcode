525ff963fba9f398ba86bf4a4c1fcc9aa6268b58
Shunpoco <tkngsnsk313320@gmail.com>
2021-09-03 22:59:33 +0900

    reverse integer on Rust

--- reverse-integer/only_i32/src/main.rs
+++ reverse-integer/only_i32/src/main.rs
@@ -2,25 +2,21 @@ struct Solution {}
 
 impl Solution {
     pub fn reverse(x: i32) -> i32 {
-        let mut result:i32 = 0;
-        let mut mx = x;
-        loop {
+        let mut result = 0i32;
+        let mut x2 = x;
+        
+        while x2 != 0 {
             match result.checked_mul(10) {
-                Some(v) => result = v,
                 None => return 0,
+                Some(v) => result = v
             }
-            
-            match result.checked_add(mx%10) {
-                Some(v) => result = v,
+            match result.checked_add(x2 % 10) {
                 None => return 0,
+                Some(v) => result = v
             }
-            
-            mx = mx / 10;
-            
-            if mx == 0 {
-                break;
-            }
+            x2 = x2 / 10;
         }
+        
         result
     }
 }

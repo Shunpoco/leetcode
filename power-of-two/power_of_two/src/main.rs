701a8b7cdcbbdfe6c2f02ed13637c83fad94abbc
Shunpoco <tkngsnsk313320@gmail.com>
2021-09-15 23:07:55 +0900

    power of two

---
+++ power-of-two/power_of_two/src/main.rs
@@ -0,0 +1,26 @@
+struct Solution {}
+
+impl Solution {
+    pub fn is_power_of_two(n: i32) -> bool {
+        if n <= 0 {
+            return false;
+        }
+        let s = format!("{:b}", n);
+                
+        let char_vec: Vec<char> = s.chars().collect();
+        
+        let mut one_count = 0usize;
+        
+        for c in char_vec {
+            if c == '1' {
+                one_count += 1;
+            }
+        }
+        
+        one_count == 1        
+    }
+}
+
+fn main() {
+    println!("{:?}", Solution::is_power_of_two(2));
+}

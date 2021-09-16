8022633b4cf46d820ab2b02612a90e4ddefcbf2c
Shunpoco <tkngsnsk313320@gmail.com>
2021-09-16 11:05:53 +0900

    bit manipulation

--- power-of-two/power_of_two/src/main.rs
+++ power-of-two/power_of_two/src/main.rs
@@ -1,24 +1,52 @@
 struct Solution {}
 
+// use string
+// impl Solution {
+//     pub fn is_power_of_two(n: i32) -> bool {
+//         if n <= 0 {
+//             return false;
+//         }
+//         let s = format!("{:b}", n);
+                
+//         let char_vec: Vec<char> = s.chars().collect();
+        
+//         let mut one_count = 0usize;
+//         for c in char_vec {
+//             if c == '1' {
+//                 one_count += 1;
+//             }
+//         }
+        
+//         one_count == 1        
+//     }
+// }
+
+// use bit manipulation
 impl Solution {
     pub fn is_power_of_two(n: i32) -> bool {
         if n <= 0 {
             return false;
         }
-        let s = format!("{:b}", n);
-                
-        let char_vec: Vec<char> = s.chars().collect();
+        
+        let mut n = n;
         
         let mut one_count = 0usize;
-        for c in char_vec {
-            if c == '1' {
+        
+        while n > 0 {
+            if n & 1 == 1 {
                 one_count += 1;
             }
+            
+            if one_count > 1 {
+                return false;
+            }
+            n = n >> 1;
         }
         
-        one_count == 1        
+        true
     }
 }
+
 
 fn main() {
     println!("{:?}", Solution::is_power_of_two(2));

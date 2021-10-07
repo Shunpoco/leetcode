ff114a6c2ee89308d419f30add53eeb0a54f7b83
Shunpoco <tkngsnsk313320@gmail.com>
2021-10-07 09:55:37 +0900

    shuffle the array

---
+++ shuffle-the-array/README.md
@@ -0,0 +1,2 @@
+## Shuffle the array
+https://leetcode.com/problems/shuffle-the-array/
---
+++ shuffle-the-array/shuffle_array/src/main.rs
@@ -0,0 +1,25 @@
+struct Solution {}
+
+impl Solution {
+    pub fn shuffle(nums: Vec<i32>, n: i32) -> Vec<i32> {
+        let n = n as usize;
+        if nums.len() != 2*n {
+            return vec![];
+        }
+        let n1 = &nums[..n];
+        let n2 = &nums[n..];
+        
+        let mut result = vec![];
+        
+        for (v1, v2) in n1.iter().zip(n2.iter()) {
+            result.push(*v1);
+            result.push(*v2);
+        }
+        
+        result
+    }
+}
+
+fn main() {
+    println!("{:?}", Solution::shuffle(vec![1,2,3,1,2,3], 3));
+}

32b15625693abd6c9609d73b12f13ace86b2ab8d
Shunpoco <tkngsnsk313320@gmail.com>
2021-12-20 18:08:09 +0900

    palindrome number

--- palindrome-number/palindrome_number/src/main.rs
+++ palindrome-number/palindrome_number/src/main.rs
@@ -2,28 +2,27 @@ struct Solution {}
 
 impl Solution {
     pub fn is_palindrome(x: i32) -> bool {
-        if x < 0 || (x > 0 && x % 10 == 0){
-            return false;
+        if x < 0 || (x > 0 && x % 10 == 0) {
+            return false
         }
         
         let mut mx = x;
-        let mut rev = 0i32;
+        let mut r = 0i32;
         
         while mx > 0 {
-            match rev.checked_mul(10) {
+            match r.checked_mul(10) {
                 None => return false,
-                Some(v) => rev = v,
+                Some(v) => r = v,
+            }
+            match r.checked_add(mx%10) {
+                None => return false,
+                Some(v) => r = v,
             }
             
-            match rev.checked_add(mx%10) {
-                None => return false,
-                Some(v) => rev = v,
-            }
-            
-            mx = mx / 10;
+            mx /= 10;
         }
-
-        rev == x
+        
+        r == x
     }
 }
 

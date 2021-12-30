bce4ce3ec874830c1d8dc94e3eb41c349d460664
Shunpoco <tkngsnsk313320@gmail.com>
2021-12-30 23:16:10 +0900

    Valid parenthesis on Rust another way

--- valid-parentheses/valid-parentheses/src/main.rs
+++ valid-parentheses/valid-parentheses/src/main.rs
@@ -2,22 +2,64 @@ use std::collections::HashMap;
 
 struct Solution {}
 
+// impl Solution {
+//     pub fn is_valid(s: String) -> bool {
+//         let matches: HashMap<char, char> = [(']', '['), ('}', '{'), (')', '(')].iter().cloned().collect();
+        
+//         let mut stack: Vec<char> = Vec::new();
+        
+//         for c in s.chars() {
+//             let l = stack.len();
+//             match matches.get(&c) {
+//                 Some(v) => {
+//                     if l == 0 || v != &stack[l-1] {
+//                         return false;
+//                     }
+//                     stack.pop();
+//                 },
+//                 _ => stack.push(c),
+//             }
+//         }
+        
+//         if stack.len() > 0 {
+//             return false;
+//         }
+        
+//         true    
+//     }
+// }
+
 impl Solution {
     pub fn is_valid(s: String) -> bool {
-        let matches: HashMap<char, char> = [(']', '['), ('}', '{'), (')', '(')].iter().cloned().collect();
+        let s:Vec<char> = s.chars().collect();
+        let h:HashMap<char, char> = [
+            ('(', ')'),
+            ('[', ']'),
+            ('{', '}'),
+        ].iter().cloned().collect();
         
-        let mut stack: Vec<char> = Vec::new();
+        let mut stack: Vec<char> = vec![];
         
-        for c in s.chars() {
-            let l = stack.len();
-            match matches.get(&c) {
-                Some(v) => {
-                    if l == 0 || v != &stack[l-1] {
-                        return false;
-                    }
-                    stack.pop();
+        for c in s {
+            match h.get(&c) {
+                Some(_) => {
+                    stack.push(c);
                 },
-                _ => stack.push(c),
+                None => {
+                    if stack.len() == 0 {
+                        return false;
+                    } else {
+                        let v = stack.pop().unwrap();
+                        match h.get(&v) {
+                            None => return false,
+                            Some(v2) => {
+                                if *v2 != c {
+                                    return false;
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
         
@@ -25,9 +67,10 @@ impl Solution {
             return false;
         }
         
-        true    
+        true
     }
 }
+
 
 fn main() {
     Solution::is_valid("()".to_string());

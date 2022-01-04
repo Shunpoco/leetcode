29d09308205118475390e1fee00791ee156c6b55
Shunpoco <tkngsnsk313320@gmail.com>
2022-01-04 23:04:28 +0900

    three sum

--- three-sum/main.py
+++ three-sum/main.py
@@ -1,26 +1,26 @@
+from typing import List
+
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums = sorted(nums)
-        
+        nums.sort()
         result = []
-        
         for i, num in enumerate(nums):
             if num > 0:
                 break
-            if i > 0 and num == nums[i-1]:
-                continue
-            result.extend(self.twoSum(nums[i+1:], -nums[i]))
-            
+            if i == 0 or num != nums[i-1]:
+                result.extend(self.twoSum(nums[i+1:], -num))
         return result
     
     def twoSum(self, nums: List[int], target: int) -> List[List[int]]:
-        memory = {}
         result = []
+        h = {}
+        
         for i, num in enumerate(nums):
-            if memory.get(num) is not None:
-                result.append([-target, memory[num], num])
-                del memory[num]
-            elif i == 0 or num != nums[i-1]:
-                memory[target-num] = num
-                
+            if h.get(num) is not None:
+                result.append([-target, h.get(num), num])
+                del h[num]
+            
+            if i == 0 or num != nums[i-1]:
+                h[target-num] = num
+        
         return result

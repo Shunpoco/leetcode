831009041261fc79805b33e673f15708113bfe11
Shunpoco <tkngsnsk313320@gmail.com>
2022-01-11 21:18:19 +0900

    fix go

--- divide-two-integers/main.go
+++ divide-two-integers/main.go
@@ -1,41 +1,43 @@
+package solution
+
 func divide(dividend int, divisor int) int {
-    positive := (dividend < 0) == (divisor < 0)
-    
-    MAX := 2147483647
-    MIN := -2147483648
-    
-    if dividend > 0 {
-        dividend = -dividend
-    }
-    if divisor > 0 {
-        divisor = -divisor
-    }
-    
-    result := 0
-    
-    for dividend <= divisor {
-        temp := divisor
-        i := 1
-        for dividend <= temp {
-            dividend -= temp
-            result += i
-            temp = temp << 1
-            i = i << 1            
-        }
-    }
-    
-    if positive == false {
-        result = -result
-    }
-   
-    if result < MIN {
-        result = MIN
-    }
-    
-    if result > MAX {
-        result = MAX
-    }
-    
-    return result
-    
+	positive := (dividend < 0) == (divisor < 0)
+
+	MAX := 2147483647
+	MIN := -2147483648
+
+	if dividend > 0 {
+		dividend = -dividend
+	}
+	if divisor > 0 {
+		divisor = -divisor
+	}
+
+	result := 0
+
+	for dividend <= divisor {
+		temp := divisor
+		i := 1
+		for dividend <= temp {
+			dividend -= temp
+			result += i
+			temp = temp << 1
+			i = i << 1
+		}
+	}
+
+	if positive == false {
+		result = -result
+	}
+
+	if result < MIN {
+		result = MIN
+	}
+
+	if result > MAX {
+		result = MAX
+	}
+
+	return result
+
 }

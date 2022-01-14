b1ca5bd26938243a9099dc557dc4b1c8ad20714c
Shunpoco <tkngsnsk313320@gmail.com>
2022-01-14 23:52:37 +0900

    unique paths 2

--- unique-paths-2/main.py
+++ unique-paths-2/main.py
@@ -1,22 +1,31 @@
+from typing import List, Tuple, Dict
+
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        memory = {}
+        mem = {}
         
-        return self.paths(obstacleGrid, 0, 0, memory)
+        return self.paths(obstacleGrid, 0, 0, mem)
     
-    def paths(self, grid: List[List[int]], m: int, n: int, memory: Dict) -> int:
-        if memory.get((m, n)) is not None:
-            return memory.get((m, n))            
+    def paths(self, grid: List[List[int]], m: int, n: int, mem: Dict[Tuple[int, int], int]) -> int:        
+        if mem.get((m, n)) is not None:
+            return mem.get((m, n))
+        
+        lm = len(grid)
+        ln = len(grid[0])
 
-        if m >= len(grid) or n >= len(grid[0]) or grid[m][n] == 1:
+        if m >= lm or n >= ln or grid[m][n] == 1:
             v = 0
-        elif m+1 == len(grid) and n+1 == len(grid[0]):
-            v = 1
-        else:
-            v1 = self.paths(grid, m+1, n, memory)
-            v2 = self.paths(grid, m, n+1, memory)
-            v = v1 + v2
-                
-        memory[(m, n)] = v
             
+        elif m+1 == lm and n+1 == ln:
+            v = 1
+            
+        else:
+            v1 = self.paths(grid, m+1, n, mem)
+            v2 = self.paths(grid, m, n+1, mem)
+            
+            v = v1 + v2
+            
+            
+        mem[(m, n)] = v
+        
         return v

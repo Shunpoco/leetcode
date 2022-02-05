a4ac700ba4921a6735b3a16e752462d2f29e7711
Shunpoco <tkngsnsk313320@gmail.com>
2022-02-05 14:20:08 +0900

    Permutation using backtrack on Python

--- permutations/python/permutations.py
+++ permutations/python/permutations.py
@@ -1,22 +1,52 @@
 from typing import List
 
+# class Solution:
+#     def permute(self, nums: List[int]) -> List[List[int]]:
+#         stack = [([], 0)]
+#         result = []
+#         L = len(nums)
+        
+#         while stack:
+#             p, l = stack.pop(-1)
+            
+#             for num in nums:
+#                 if num not in p:
+#                     t = p.copy()
+#                     t.append(num)
+                    
+#                     if len(t) == L:
+#                         result.append(t)
+#                     else:
+#                         stack.append([t, l+1])
+                        
+#         return result
+
+
+# Using backtrack
+from collections import Counter
+
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        stack = [([], 0)]
-        result = []
-        L = len(nums)
+        c = Counter()
         
-        while stack:
-            p, l = stack.pop(-1)
+        for num in nums:
+            c[num] += 1
             
-            for num in nums:
-                if num not in p:
-                    t = p.copy()
-                    t.append(num)
-                    
-                    if len(t) == L:
-                        result.append(t)
-                    else:
-                        stack.append([t, l+1])
-                        
+        result = []
+
+        self.backtrack(nums, [], result, c)
+        
         return result
+
+    def backtrack(self, nums: List[int], v: List[int], result: List[List[int]], c: Counter):
+        if len(v) == len(nums):
+            result.append(v.copy())
+            return
+        
+        for num in nums:
+            if c[num] -1 >= 0:
+                v.append(num)
+                c[num] -= 1
+                self.backtrack(nums, v, result, c)
+                c[num] += 1
+                v.pop(-1)

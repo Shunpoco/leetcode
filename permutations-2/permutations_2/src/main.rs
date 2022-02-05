c19bd5d2109d07e2438e29dda155ac975c5e0169
Shunpoco <tkngsnsk313320@gmail.com>
2022-02-05 15:18:35 +0900

    permutation2 with backtrack on Rust

--- permutations-2/permutations_2/src/main.rs
+++ permutations-2/permutations_2/src/main.rs
@@ -1,47 +1,83 @@
-struct Solution;
+use std::collections::HashMap;
 
+struct Solution;
+// Backtrack
 impl Solution {
     pub fn permute_unique(nums: Vec<i32>) -> Vec<Vec<i32>> {
-        let mut nums = nums;
-        nums.sort();
         let l = nums.len();
-        
-        let mut counter = vec![0;21];
+        let mut counter = HashMap::new();
         for num in &nums {
-            counter[(num+10) as usize] += 1;
+            *counter.entry(num).or_insert(0) += 1;
         }
-        
-        let mut stack = vec![(vec![], counter)];
+        let keys:Vec<&i32> = counter.keys().cloned().collect();
         let mut result = vec![];
-                
-        while stack.len() > 0 {
-            let vs = stack.pop().unwrap();
-            let now = vs.0;
-            let cur_counter = vs.1;
-            
-            if now.len() == l {
-                result.push(now);
-                continue;
-            }
-            
-            for i in 0..l {
-                let num = nums[i];
-                
-                if (i == 0 || nums[i-1] != num) && cur_counter[(num+10) as usize] > 0 {
-                    let mut t_now = now.clone();
-                    t_now.push(num);
-                    let mut t_cur_counter = cur_counter.clone();
-                    t_cur_counter[(num+10) as usize] -= 1;
-                    
-                    stack.push((t_now, t_cur_counter));
-                }
-            }
-            
-        }
+        Solution::backtrack(l, &keys, &mut result, &mut vec![], &mut counter);
         
         result
     }
+    
+    fn backtrack(length: usize, nums: &Vec<&i32>, result: &mut Vec<Vec<i32>>, current: &mut Vec<i32>, counter: &mut HashMap<&i32, i32>) {
+        if length == current.len() {
+            result.push((*current).clone());
+            return;
+        }
+        
+        for num in nums {
+            if *counter.get(*num).unwrap() -1 >= 0 {
+                *counter.get_mut(*num).unwrap() -= 1;
+                current.push(**num);
+                Solution::backtrack(length, nums, result, current, counter);
+                current.pop();
+                *counter.get_mut(*num).unwrap() += 1;
+            }
+        }
+    }
 }
+
+
+
+// impl Solution {
+//     pub fn permute_unique(nums: Vec<i32>) -> Vec<Vec<i32>> {
+//         let mut nums = nums;
+//         nums.sort();
+//         let l = nums.len();
+        
+//         let mut counter = vec![0;21];
+//         for num in &nums {
+//             counter[(num+10) as usize] += 1;
+//         }
+        
+//         let mut stack = vec![(vec![], counter)];
+//         let mut result = vec![];
+                
+//         while stack.len() > 0 {
+//             let vs = stack.pop().unwrap();
+//             let now = vs.0;
+//             let cur_counter = vs.1;
+            
+//             if now.len() == l {
+//                 result.push(now);
+//                 continue;
+//             }
+            
+//             for i in 0..l {
+//                 let num = nums[i];
+                
+//                 if (i == 0 || nums[i-1] != num) && cur_counter[(num+10) as usize] > 0 {
+//                     let mut t_now = now.clone();
+//                     t_now.push(num);
+//                     let mut t_cur_counter = cur_counter.clone();
+//                     t_cur_counter[(num+10) as usize] -= 1;
+                    
+//                     stack.push((t_now, t_cur_counter));
+//                 }
+//             }
+            
+//         }
+        
+//         result
+//     }
+// }
 
 fn main() {
     assert_eq!(Solution::permute_unique(vec![1,1,2]), vec![vec![2,1,1], vec![1,2,1], vec![1,1,2]]);

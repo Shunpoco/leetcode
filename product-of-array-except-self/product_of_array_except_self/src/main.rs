708c6970bd084625efa5894b322685f0d6bcac66
Shunpoco <tkngsnsk313320@gmail.com>
2022-04-15 22:18:22 +0900

    improved

--- product-of-array-except-self/product_of_array_except_self/src/main.rs
+++ product-of-array-except-self/product_of_array_except_self/src/main.rs
@@ -1,36 +1,22 @@
-use std::collections::HashMap;
-
 struct Solution;
 impl Solution {
     pub fn product_except_self(nums: Vec<i32>) -> Vec<i32> {
         let mut result = vec![];
-        let mut memory = HashMap::new();
-        memory.insert(nums.len(), 1);
         
-        let mut v = 1i32;
-        for i in 0..nums.len() {
-            result.push(v*exec(&nums, i+1, &mut memory));
-            v *= nums[i];
+        let mut v = 1;
+        for &num in nums.iter() {
+            result.push(v);
+            v *= num;
         }
         
-        result        
+        v = 1;
+        for i in 0..nums.len() {
+            result[nums.len()-1-i] *= v;
+            v *= nums[nums.len()-1-i];
+        }
+        
+        result
     }
-}
-
-fn exec(nums: &Vec<i32>, idx: usize, memory: &mut HashMap<usize, i32>) -> i32 {
-    if let Some(v) = memory.get(&idx) {
-        return *v;
-    }
-    let result:i32;
-    if idx == nums.len()-1 {
-        result = nums[idx];
-    } else {
-        result = nums[idx] * exec(nums, idx+1, memory);
-    }
-    
-    memory.insert(idx, result);
-    
-    result
 }
 
 fn main() {

b38186c8a060596a02570ffaf77402fbb666db9c
Shunpoco <tkngsnsk313320@gmail.com>
2022-04-18 01:00:47 +0900

    a

--- product-of-array-except-self/product_of_array_except_self/src/main.rs
+++ product-of-array-except-self/product_of_array_except_self/src/main.rs
@@ -1,16 +1,17 @@
 struct Solution;
 impl Solution {
     pub fn product_except_self(nums: Vec<i32>) -> Vec<i32> {
-        let n = nums.len();
-        let mut result = vec![1;n];
-        let mut left = 1i32;
-        let mut right = 1i32;
-        for i in 0..n {
+        let l = nums.len();
+        let mut result = vec![1;l];
+        
+        let (mut left, mut right) = (1, 1);
+        for i in 0..nums.len() {
             result[i] *= left;
-            result[n-1-i] *= right;
+            result[l-1-i] *= right;
             left *= nums[i];
-            right *= nums[n-1-i];
+            right *= nums[l-1-i];
         }
+        
         result
     }
 }

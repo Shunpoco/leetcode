5d1d1dfcf1c29ed04ec7e7464ef1c558a20cb789
Shunpoco <tkngsnsk313320@gmail.com>
2022-04-29 11:55:07 +0900

    longest repeating character replacement on Rust

---
+++ longest-repeating-character-replacement/README.md
@@ -0,0 +1,2 @@
+## Longest repeating character replacement
+https://leetcode.com/problems/longest-repeating-character-replacement/
---
+++ longest-repeating-character-replacement/longest_repeating_character_replacement/src/main.rs
@@ -0,0 +1,32 @@
+struct Solution;
+impl Solution {
+    pub fn character_replacement(s: String, k: i32) -> i32 {
+        let s: Vec<char> = s.chars().collect();
+        let l = s.len();
+        let mut lo = 0usize;
+        let mut hi = 0usize;
+        let mut mo = 0usize;
+        let mut memory = vec![0;26];
+        let mut result = 0usize;
+        
+        while hi < l {
+            let v = &mut memory[s[hi] as usize - 65];
+            *v += 1;
+            mo = std::cmp::max(mo, *v);
+            
+            while hi-lo+1-mo > (k as usize) {
+                memory[s[lo] as usize - 65] -= 1;
+                lo += 1;
+            }
+            
+            result = std::cmp::max(result, hi-lo+1);
+            hi += 1;
+        }
+        
+        result as i32
+    }
+}
+
+fn main() {
+    assert_eq!(Solution::character_replacement("ABAB".to_string(), 2), 4);
+}

0f5c7b53f14cad4c1f34a44ae18ccd2ee8ddabfc
Shunpoco <tkngsnsk313320@gmail.com>
2022-04-30 16:57:14 +0900

    min cost climbing stairs using 3 vals

--- min-cost-climbing-stairs/min_cost_climbing_stairs/src/main.rs
+++ min-cost-climbing-stairs/min_cost_climbing_stairs/src/main.rs
@@ -1,16 +1,29 @@
 struct Solution;
+// impl Solution {
+//     pub fn min_cost_climbing_stairs(cost: Vec<i32>) -> i32 {
+//         let mut mem = vec![0;cost.len()+1];
+//         for i in 0..mem.len() {
+//             if i == 0 || i == 1 {
+//                 mem[i] = 0;
+//             } else {
+//                 mem[i] = std::cmp::min(mem[i-2]+cost[i-2], mem[i-1]+cost[i-1]);
+//             }
+//         }
+        
+//         mem[mem.len()-1]
+//     }
+// }
+
 impl Solution {
     pub fn min_cost_climbing_stairs(cost: Vec<i32>) -> i32 {
-        let mut mem = vec![0;cost.len()+1];
-        for i in 0..mem.len() {
-            if i == 0 || i == 1 {
-                mem[i] = 0;
-            } else {
-                mem[i] = std::cmp::min(mem[i-2]+cost[i-2], mem[i-1]+cost[i-1]);
-            }
-        }
+        let (mut v1, mut v2, mut v3) = (0, 0, 0);
         
-        mem[mem.len()-1]
+        for i in 2..cost.len()+1 {
+            v3 = std::cmp::min(v2+cost[i-1], v1+cost[i-2]);            
+            v1 = v2;
+            v2 = v3;
+        }        
+        v3
     }
 }
 

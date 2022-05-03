905cfd6a951956bb42a27110efdda85c57a9ad28
Shunpoco <tkngsnsk313320@gmail.com>
2022-05-03 12:52:48 +0900

    Modify

--- shortest-unsorted-continuous-subarray/shortest_unsorted_continuous_subarray/src/main.rs
+++ shortest-unsorted-continuous-subarray/shortest_unsorted_continuous_subarray/src/main.rs
@@ -1,36 +1,34 @@
 struct Solution;
 impl Solution {
     pub fn find_unsorted_subarray(nums: Vec<i32>) -> i32 {
+        let mut right = 0usize;
         let mut max = nums[0];
         let mut min = nums[0];
-        let mut i_max = 0;
-        let mut left = -1;
-        let mut right = -1;
+        let mut decrease = false;
         
         for i in 1..nums.len() {
-            if max < nums[i] {
+            if max <= nums[i] {
                 max = nums[i];
-                i_max = i;
-            }
-            if max > nums[i] {
-                if left == -1 {
-                    left = i_max as i32;
-                    right = i as i32;
+            } else {
+                if !decrease {
+                    decrease = true;
+                    right = i;
                     min = nums[i];
                 } else {
-                    right = i as i32;
+                    right = i;
                     if min > nums[i] {
                         min = nums[i];
                     }
                 }
             }
         }
-        let mut new_left = 0i32;
-        while new_left < right && nums[new_left as usize] <= min {
-            new_left += 1;
+        
+        let mut left = 0usize;
+        while left < right && nums[left] <= min {
+            left += 1;
         }
         
-        if right == new_left { 0 } else { right-new_left+1 }
+        if left == right { 0 } else { (right-left+1) as i32 }
     }
 }
 

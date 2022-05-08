824f6744141924c0f9cce6a931b9d537b6c6e4ec
Shunpoco <tkngsnsk313320@gmail.com>
2022-05-08 17:37:13 +0900

    intersection of multiple arrays using hashmap

--- intersection-of-multiple-arrays/intersection_of_multiple_arrays/src/main.rs
+++ intersection-of-multiple-arrays/intersection_of_multiple_arrays/src/main.rs
@@ -1,25 +1,50 @@
+use std::collections::HashMap;
+
 struct Solution;
 impl Solution {
     pub fn intersection(nums: Vec<Vec<i32>>) -> Vec<i32> {
-        let mut memory = vec![0;1001];
+        let mut memory = HashMap::new();
         
         for num in &nums {
             for &n in num {
-                memory[n as usize] += 1;
+                *memory.entry(n).or_insert(0) += 1;
             }
         }
         
         let mut result = vec![];
         let l = nums.len();
-        for (i, &m) in memory.iter().enumerate() {
-            if m == l {
-                result.push(i as i32);
+        for (k, v) in memory {
+            if v == l {
+                result.push(k as i32);
             }
         }
+        result.sort();
         
         result
     }
 }
+
+// impl Solution {
+//     pub fn intersection(nums: Vec<Vec<i32>>) -> Vec<i32> {
+//         let mut memory = vec![0;1001];
+        
+//         for num in &nums {
+//             for &n in num {
+//                 memory[n as usize] += 1;
+//             }
+//         }
+        
+//         let mut result = vec![];
+//         let l = nums.len();
+//         for (i, &m) in memory.iter().enumerate() {
+//             if m == l {
+//                 result.push(i as i32);
+//             }
+//         }
+        
+//         result
+//     }
+// }
 
 
 fn main() {

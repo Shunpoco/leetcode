05ab168c3607d13b1d9e842d23c59d8fcd6fdfba
Shunpoco <tkngsnsk313320@gmail.com>
2022-05-26 21:58:32 +0900

    longeset increasing subsequecne (again) using 1-dim array dp

--- longest-increasing-subsequence/longest_increasing_subsequence/src/main.rs
+++ longest-increasing-subsequence/longest_increasing_subsequence/src/main.rs
@@ -3,37 +3,23 @@ use std::collections::HashMap;
 struct Solution;
 impl Solution {
     pub fn length_of_lis(nums: Vec<i32>) -> i32 {
-        let mut memory = HashMap::new();
+        let mut dp = vec![];
         
-        dp(&nums, -10001, 0, &mut memory)
-    }
-}
-
-fn dp(nums: &Vec<i32>, val: i32, idx: usize, memory: &mut HashMap<usize, i32>) -> i32 {
-    if let Some(v) = memory.get(&idx) {
-        return *v;
-    }
-    
-    if idx == nums.len() {
-        return 0;
-    }
-    
-    let mut count = 0i32;
-    for i in idx..nums.len() {
-        if val < nums[i] {
-            let v = 1 + dp(nums, nums[i], i+1, memory);
-            
-            if v > count {
-                count = v;
+        for &num in nums.iter() {
+            if let Some(i) = dp.binary_search(&num).err() {
+                if i >= dp.len() {
+                    dp.push(num);
+                } else {
+                    dp[i] = num;
+                }
             }
         }
+        
+        // println!("{:?}", dp);
+        
+        dp.len() as i32
     }
-    
-    memory.insert(idx, count);
-    
-    count
 }
-
 
 fn main() {
     assert_eq!(Solution::length_of_lis(vec![0,1,0,3,2,3]), 4);

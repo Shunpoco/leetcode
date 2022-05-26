631cf5dd2e38116fce45bd0215a417c7b70b5f04
Shunpoco <tkngsnsk313320@gmail.com>
2022-05-26 14:38:23 +0900

    number of 1bit on Go again

--- number-of-1bits/nob.go
+++ number-of-1bits/nob.go
@@ -3,11 +3,11 @@ package number
 func hammingWeight(num uint32) int {
 	result := 0
 	for num > 0 {
-		temp := num & 1
-		if temp == 1 {
+		if num&1 == 1 { // also num%2 == 1
 			result++
 		}
-		num = num >> 1
+		num >>= 1
 	}
+
 	return result
 }

3927ece153d50547fe10cbb74dfe9abbb311b4ce
Shunpoco <tkngsnsk313320@gmail.com>
2022-06-02 13:56:44 +0900

    transpose matrix with initialize

--- transpose-matrix/main.go
+++ transpose-matrix/main.go
@@ -2,10 +2,13 @@ package main
 
 func transpose(matrix [][]int) [][]int {
 	res := make([][]int, len(matrix[0]))
+	for i := 0; i < len(res); i++ {
+		res[i] = make([]int, len(matrix))
+	}
 
 	for c := 0; c < len(matrix[0]); c++ {
 		for r := 0; r < len(matrix); r++ {
-			res[c] = append(res[c], matrix[r][c])
+			res[c][r] = matrix[r][c]
 		}
 	}
 

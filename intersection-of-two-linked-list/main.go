deebe3d16d5a408e53893e273d6ef4c8ed1668b8
Shunpoco <tkngsnsk313320@gmail.com>
2022-06-06 21:17:51 +0900

    intersection of two linked list on Go again

--- intersection-of-two-linked-list/main.go
+++ intersection-of-two-linked-list/main.go
@@ -1,30 +1,46 @@
 package main
 
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
+// Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
 func getIntersectionNode(headA, headB *ListNode) *ListNode {
-    l := [](*ListNode){}
-    cur := headA
-    for cur != nil {
-        l = append(l, cur)
-        cur = (*cur).Next
-    }
-    
-    cur = headB
-    for cur != nil {
-        for _, n := range l {
-            if cur == n {
-                return cur
-            }
-        }
-        
-        cur = (*cur).Next
-    }
-    
-    return nil
+	curA := headA
+	lenA := 0
+	for curA != nil {
+		lenA++
+		curA = (*curA).Next
+	}
+
+	curB := headB
+	lenB := 0
+	for curB != nil {
+		lenB++
+		curB = (*curB).Next
+	}
+
+	curA = headA
+	curB = headB
+
+	for lenA > lenB {
+		curA = (*curA).Next
+		lenA--
+	}
+
+	for lenB > lenA {
+		curB = (*curB).Next
+		lenB--
+	}
+
+	for curA != nil && curB != nil {
+		if curA == curB {
+			return curA
+		}
+		curA = (*curA).Next
+		curB = (*curB).Next
+	}
+
+	return nil
 }

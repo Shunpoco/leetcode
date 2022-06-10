edd5177cb56d28ab9722ffcbbce57b75d424cc4a
Shunpoco <tkngsnsk313320@gmail.com>
2022-06-11 01:34:17 +0900

    zigzag with 1-dim slice

--- zigzag-conversion/main.go
+++ zigzag-conversion/main.go
@@ -3,29 +3,26 @@ package main
 import "fmt"
 
 func convert(s string, numRows int) string {
-    if numRows == 1 {
-        return s
-    }
-    
-    mod := numRows + (numRows - 2)
-    result := make([][]rune, numRows)
-    for i := 0; i < numRows; i++ {
-        result[i] = []rune{}
-    }
-    
-    for i, r := range s {
-        m := i % mod
-        if m < numRows {
-            result[m] = append(result[m], r)
-        } else {
-            result[(numRows-1)-(m-(numRows-1))] = append(result[(numRows-1)-(m-(numRows-1))], r)
-        }
-    }
-        
-    res := ""
-    for _, row := range result {
-        res = fmt.Sprintf("%s%s", res, string(row))
-    }
-    
-    return res
+	if numRows == 1 {
+		return s
+	}
+
+	mod := numRows + (numRows - 2)
+	result := make([]string, numRows)
+
+	for i, r := range s {
+		m := i % mod
+		if m < numRows {
+			result[m] = fmt.Sprintf("%s%s", result[m], string(r))
+		} else {
+			result[(numRows-1)-(m-(numRows-1))] = fmt.Sprintf("%s%s", result[(numRows-1)-(m-(numRows-1))], string(r))
+		}
+	}
+
+	res := ""
+	for _, row := range result {
+		res = fmt.Sprintf("%s%s", res, row)
+	}
+
+	return res
 }

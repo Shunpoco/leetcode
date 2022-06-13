e67102c2137c57b22cbbae992962d9f78b86d888
Shunpoco <tkngsnsk313320@gmail.com>
2022-06-13 21:39:04 +0900

    modify bitwise and of numbers range on Go

--- bitwise-and-of-numbers-range/main.go
+++ bitwise-and-of-numbers-range/main.go
@@ -1,31 +1,23 @@
 package main
 
 func rangeBitwiseAnd(left int, right int) int {
-    result := 0
-    
-    for left > 0 && right > 0 {
-        b_l := 1
-        for b_l <= left {
-            b_l *= 2
-        }
-        if b_l > left {
-            b_l /= 2
-        }
-        b_r := 1
-        for b_r < right {
-            b_r *= 2
-        }
-        if b_r > right {
-            b_r /= 2
-        }
-        
-        if b_l != b_r {
-            break
-        }
-        result += b_l
-        left -= b_l
-        right -= b_r
-    }
-    
-    return result
+	result := 0
+
+	for left > 0 && right > 0 {
+		b := 1
+		for b <= left {
+			b *= 2
+		}
+		if b > left {
+			b /= 2
+		}
+		if b*2 <= right {
+			break
+		}
+		result += b
+		left -= b
+		right -= b
+	}
+
+	return result
 }

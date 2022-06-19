52cbef6d570d53ae329c0d81ecff6e8f6b5ab0f5
Shunpoco <tkngsnsk313320@gmail.com>
2022-06-19 14:23:44 +0900

    modify search suggestions system on Go

--- search-suggestions-system/main.go
+++ search-suggestions-system/main.go
@@ -3,35 +3,37 @@ package main
 import "sort"
 
 func suggestedProducts(products []string, searchWord string) [][]string {
-    sort.Slice(products, func(i, j int) bool { return products[i] < products[j] })
-    memory := invertedIndex(&products)
-    
-    result := [][]string{}
-    for i := 1; i <= len(searchWord); i++ {
-        prefix := searchWord[:i]
-        candidates := memory[prefix]
-        t := []string{}
-        for j := 0; j < len(candidates) && j < 3; j++ {
-            t = append(t, products[candidates[j]])
-        }        
-        result = append(result, t)
-    }
-    
-    return result
+	n := len(searchWord)
+	capResult := 3
+	sort.Slice(products, func(i, j int) bool { return products[i] < products[j] })
+	memory := invertedIndex(&products, n)
+
+	result := [][]string{}
+	for i := 1; i <= n; i++ {
+		prefix := searchWord[:i]
+		candidates := memory[prefix]
+		t := []string{}
+		for j := 0; j < len(candidates) && j < capResult; j++ {
+			t = append(t, products[candidates[j]])
+		}
+		result = append(result, t)
+	}
+
+	return result
 }
 
-func invertedIndex(words *[]string) map[string][]int {
-    result := make(map[string][]int)
-    
-    for idx, word := range *words {
-        for i := 1; i <= len(word); i++ {
-            if _, ok := result[word[:i]]; ok {
-                result[word[:i]] = append(result[word[:i]], idx)
-            } else {
-                result[word[:i]] = []int{idx}
-            }
-        }
-    }
-    
-    return result
+func invertedIndex(words *[]string, capN int) map[string][]int {
+	result := make(map[string][]int)
+
+	for idx, word := range *words {
+		for i := 1; i <= len(word) && i <= capN; i++ {
+			if _, ok := result[word[:i]]; ok {
+				result[word[:i]] = append(result[word[:i]], idx)
+			} else {
+				result[word[:i]] = []int{idx}
+			}
+		}
+	}
+
+	return result
 }

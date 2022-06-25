5bbb7a2b6840fd8b383676f8029e356c15571d69
Shunpoco <tkngsnsk313320@gmail.com>
2022-06-25 15:16:59 +0900

    check row and column using bool

--- check-if-every-row-and-column-contains-all-numbers/main.go
+++ check-if-every-row-and-column-contains-all-numbers/main.go
@@ -1,46 +1,46 @@
 package main
 
 func checkValid(matrix [][]int) bool {
-    n := len(matrix)
-    for row := 0; row < n; row++ {
-        if !validRow(row, n, &matrix) {
-            return false
-        }
-    }
-    
-    for col := 0; col < n; col++ {
-        if !validCol(col, n, &matrix) {
-            return false
-        }
-    }
-    
-    return true
+	n := len(matrix)
+	for row := 0; row < n; row++ {
+		if !validRow(row, n, &matrix) {
+			return false
+		}
+	}
+
+	for col := 0; col < n; col++ {
+		if !validCol(col, n, &matrix) {
+			return false
+		}
+	}
+
+	return true
 }
 
 func validRow(row, n int, matrix *[][]int) bool {
-    memory := make([]int, n)
-    
-    for col := 0; col < n; col++ {
-        v := (*matrix)[row][col]
-        if memory[v-1] == -1 {
-            return false
-        }
-        memory[v-1] -= 1
-    }
-    
-    return true
+	memory := make([]bool, n)
+
+	for col := 0; col < n; col++ {
+		v := (*matrix)[row][col]
+		if memory[v-1] {
+			return false
+		}
+		memory[v-1] = true
+	}
+
+	return true
 }
 
 func validCol(col, n int, matrix *[][]int) bool {
-    memory := make([]int, n)
-    
-    for row := 0; row < n; row++ {
-        v := (*matrix)[row][col]
-        if memory[v-1] == -1 {
-            return false
-        }
-        memory[v-1] -= 1
-    }
-    
-    return true
+	memory := make([]bool, n)
+
+	for row := 0; row < n; row++ {
+		v := (*matrix)[row][col]
+		if memory[v-1] {
+			return false
+		}
+		memory[v-1] = true
+	}
+
+	return true
 }

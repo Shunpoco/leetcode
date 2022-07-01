974dafe040a2d315ea6415976bd54da3d4e93b33
Shunpoco <tkngsnsk313320@gmail.com>
2022-07-01 17:30:40 +0900

    modify maximum units on a truck on Go

--- maximum-units-on-a-truck/main.go
+++ maximum-units-on-a-truck/main.go
@@ -3,19 +3,26 @@ package main
 import "sort"
 
 func maximumUnits(boxTypes [][]int, truckSize int) int {
-    sort.Slice(boxTypes, func(i, j int) bool { return boxTypes[i][1] > boxTypes[j][1] })
-    // fmt.Println(boxTypes)
-    
-    result := 0
-    idx := 0
-    for truckSize > 0 && idx < len(boxTypes) {        
-        result += boxTypes[idx][1]
-        boxTypes[idx][0] -= 1
-        if boxTypes[idx][0] == 0 {
-            idx++
-        }
-        truckSize--
-    }
-    
-    return result
+	sort.Slice(boxTypes, func(i, j int) bool { return boxTypes[i][1] > boxTypes[j][1] })
+	// fmt.Println(boxTypes)
+
+	result := 0
+	idx := 0
+	for truckSize > 0 && idx < len(boxTypes) {
+		var d int
+		if boxTypes[idx][0] >= truckSize {
+			d = truckSize
+		} else {
+			d = boxTypes[idx][0]
+		}
+		boxTypes[idx][0] -= d
+		truckSize -= d
+
+		result += boxTypes[idx][1] * d
+		if boxTypes[idx][0] == 0 {
+			idx++
+		}
+	}
+
+	return result
 }

39171ee4977ad2b958fba8b7e44df692c375b652
Shunpoco <tkngsnsk313320@gmail.com>
2022-07-03 14:32:47 +0900

    modify wiggle subsequence

--- wiggle-subsequence/main.go
+++ wiggle-subsequence/main.go
@@ -8,38 +8,37 @@ type Tuple struct {
 func wiggleMaxLength(nums []int) int {
 	n := len(nums)
 
-	dp := make([]Tuple, 2)
-	dp[0] = Tuple{1, nums[0]}
-	dp[1] = Tuple{1, nums[0]}
+	dp0 := Tuple{1, nums[0]}
+	dp1 := Tuple{1, nums[0]}
 
 	for i := 1; i < n; i++ {
-		// dp[0] (Increase)
+		// dp0 (Increase)
 		var ti Tuple
-		if dp[1].val < nums[i] {
-			ti = Tuple{dp[1].num + 1, nums[i]}
-		} else if dp[0].val < nums[i] {
-			ti = Tuple{dp[0].num, nums[i]}
+		if dp1.val < nums[i] {
+			ti = Tuple{dp1.num + 1, nums[i]}
+		} else if dp0.val < nums[i] {
+			ti = Tuple{dp0.num, nums[i]}
 		} else {
-			ti = dp[0]
+			ti = dp0
 		}
 
-		// dp[1] (Decrease)
+		// dp1 (Decrease)
 		var td Tuple
-		if dp[0].val > nums[i] {
-			td = Tuple{dp[0].num + 1, nums[i]}
-		} else if dp[1].val > nums[i] {
-			td = Tuple{dp[1].num, nums[i]}
+		if dp0.val > nums[i] {
+			td = Tuple{dp0.num + 1, nums[i]}
+		} else if dp1.val > nums[i] {
+			td = Tuple{dp1.num, nums[i]}
 		} else {
-			td = dp[1]
+			td = dp1
 		}
 
-		dp[0] = ti
-		dp[1] = td
+		dp0 = ti
+		dp1 = td
 	}
 
-	result := dp[0].num
-	if dp[1].num > result {
-		result = dp[1].num
+	result := dp0.num
+	if dp1.num > result {
+		result = dp1.num
 	}
 
 	return result

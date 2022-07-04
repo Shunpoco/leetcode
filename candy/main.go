6fbedee1b553956d46b7a18171520e79998f8763
Shunpoco <tkngsnsk313320@gmail.com>
2022-07-04 12:07:08 +0900

    modify candy on Go

--- candy/main.go
+++ candy/main.go
@@ -1,35 +1,33 @@
 package main
 
 func candy(ratings []int) int {
-    n := len(ratings)
-    order := make([]int, n)
-    reverse := make([]int, n)
-    order[0] = 1
-    reverse[n-1] = 1
-    
-    for i := 1; i < n; i++ {
-        if ratings[i] > ratings[i-1] {
-            order[i] = order[i-1] + 1
-        } else {
-            order[i] = 1
-        }
-        if ratings[n-1-i] > ratings[n-1-(i-1)] {
-            reverse[n-1-i] = reverse[n-1-(i-1)] + 1
-        } else {
-            reverse[n-1-i] = 1
-        }
-    }
-    
-    fmt.Println(order, reverse)
-    
-    var result int
-    for i := 0; i < n; i++ {
-        v := order[i]
-        if v < reverse[i] {
-            v = reverse[i]
-        }
-        result += v
-    }
-    
-    return result
+	n := len(ratings)
+	order := make([]int, n)
+	order[0] = 1
+	order[n-1] = 1
+
+	for i := 1; i < n; i++ {
+		v := 1
+		if ratings[i] > ratings[i-1] {
+			v = order[i-1] + 1
+		}
+		if order[i] < v {
+			order[i] = v
+		}
+
+		v = 1
+		if ratings[n-1-i] > ratings[n-1-(i-1)] {
+			v = order[n-1-(i-1)] + 1
+		}
+		if order[n-1-i] < v {
+			order[n-1-i] = v
+		}
+	}
+
+	var result int
+	for _, num := range order {
+		result += num
+	}
+
+	return result
 }

93098d1061cfea02cdb7526d0689cfa429a7a53f
Shunpoco <tkngsnsk313320@gmail.com>
2022-07-06 12:16:46 +0900

    modify fibonacci number on Go

--- fibonacci-number/main.go
+++ fibonacci-number/main.go
@@ -1,24 +1,25 @@
 package main
 
 func fib(n int) int {
-    dp := make([]int, n+1)
-    return solve(n, &dp)
-    
+	dp := make([]int, n+1)
+	solve(n, &dp)
+
+	return dp[n]
 }
 
-func solve(n int, dp *[]int) int {
-    if n == 0 || (*dp)[n] != 0 {
-        return (*dp)[n]
-    }
-    
-    var result int
-    if n == 1 {
-        result = 1
-    } else {
-        result = solve(n-1, dp) + solve(n-2, dp)    
-    }
-    
-    (*dp)[n] = result
-    
-    return result
+func solve(n int, dp *[]int) {
+	if n == 0 || (*dp)[n] != 0 {
+		return
+	}
+
+	var result int
+	if n == 1 {
+		result = 1
+	} else {
+		solve(n-1, dp)
+		solve(n-2, dp)
+		result = (*dp)[n-1] + (*dp)[n-2]
+	}
+
+	(*dp)[n] = result
 }

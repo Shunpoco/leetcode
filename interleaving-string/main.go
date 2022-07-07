934ff319c04170e619ce69694c0799eef0287afd
Shunpoco <tkngsnsk313320@gmail.com>
2022-07-07 10:36:39 +0900

    modify

--- interleaving-string/main.go
+++ interleaving-string/main.go
@@ -1,9 +1,7 @@
 package main
 
 func isInterleave(s1 string, s2 string, s3 string) bool {
-	n1 := len(s1)
-	n2 := len(s2)
-	n3 := len(s3)
+	n1, n2, n3 := len(s1), len(s2), len(s3)
 
 	if n1+n2 != n3 {
 		return false

0e5d11df2e170cd208f0eef1d57785eeedd8b96f
Shunpoco <tkngsnsk313320@gmail.com>
2022-07-19 21:16:53 +0900

    pascal triangle on Go

--- pascals-triangle/pascal_triangle.go
+++ pascals-triangle/pascal_triangle.go
@@ -1,35 +1,23 @@
 package pascal
 
 func generate(numRows int) [][]int {
-	if numRows == 0 {
-		return [][]int{}
-	}
 	result := make([][]int, numRows)
-
-	temp := make([]int, 1)
-	temp[0] = 1
-	result[0] = temp
-	for i := 1; i < numRows; i++ {
-		temp = nextRow(temp)
-		result[i] = temp
+	t := []int{}
+	for i := 1; i <= numRows; i++ {
+		t = solve(i, t)
+		result[i-1] = t
 	}
 
 	return result
 }
 
-func nextRow(before []int) []int {
-	result := make([]int, len(before)+1)
+func solve(i int, t []int) []int {
+	r := make([]int, i)
+	r[0], r[i-1] = 1, 1
 
-	for i := 0; i < len(result); i++ {
-		var sum int
-		if i == 0 {
-			sum = before[0]
-		} else if i == len(before) {
-			sum = before[len(before)-1]
-		} else {
-			sum = before[i-1] + before[i]
-		}
-		result[i] = sum
+	for j := 1; j < i-1; j++ {
+		r[j] = t[j-1] + t[j]
 	}
-	return result
+
+	return r
 }

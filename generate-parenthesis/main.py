c90a2ae61d8309f9b59182c6a5df29ee7e9ad738
Shunpoco <tkngsnsk313320@gmail.com>
2022-07-20 23:16:16 +0900

    generate parenthesis on Python

--- generate-parenthesis/main.py
+++ generate-parenthesis/main.py
@@ -1,12 +1,18 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        if n == 0:
-            return [""]
-        ans = []
+        result = []
         
-        for c in range(n):
-            for left in self.generateParenthesis(c):
-                for right in self.generateParenthesis(n-1-c):
-                    ans.append(f"{left}({right})")
-                    
-        return ans
+        self.solve(n, n, "", result)
+        
+        return result
+    
+    def solve(self, left, right, result, results):
+        if left == 0 and right == 0:
+            results.append(result)
+            return
+        
+        if left > 0:
+            self.solve(left-1, right, result+"(", results)
+            
+        if right > left:
+            self.solve(left, right-1, result+")", results)

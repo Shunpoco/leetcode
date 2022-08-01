e207dff63724a18acea38530ae9a9d1a43913a68
Shunpoco <tkngsnsk313320@gmail.com>
2022-08-01 17:49:44 +0900

    modify unique paths on Go

--- unique-paths/main.go
+++ unique-paths/main.go
@@ -1,20 +1,20 @@
 package main
 
 func uniquePaths(m int, n int) int {
-	dp := make([][]int, m+1)
 
-	for i := 0; i < m+1; i++ {
-		dp[i] = make([]int, n+1)
+	dp := make([]int, n+1)
+
+	for i := 1; i < m+1; i++ {
 		for j := 0; j < n+1; j++ {
-			if i == 0 || j == 0 {
-				dp[i][j] = 0
+			if j == 0 {
+				dp[j] = 0
 			} else if i == 1 && j == 1 {
-				dp[i][j] = 1
+				dp[j] = 1
 			} else {
-				dp[i][j] = dp[i-1][j] + dp[i][j-1]
+				dp[j] = dp[j] + dp[j-1]
 			}
 		}
 	}
 
-	return dp[m][n]
+	return dp[n]
 }

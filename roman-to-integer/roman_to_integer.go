8fdba613901dced874e4459e1634ed146539799e
Shunpoco <tkngsnsk313320@gmail.com>
2022-08-15 17:18:39 +0900

    roman to integer on Go again

--- roman-to-integer/roman_to_integer.go
+++ roman-to-integer/roman_to_integer.go
@@ -16,11 +16,87 @@ func romanToInt(s string) int {
 		temp := matcher[rune(s[i])]
 		if i < len(s)-1 && temp < matcher[rune(s[i+1])] {
 			temp = matcher[rune(s[i+1])] - temp
-			i = i + 1
+			i++
 		}
 		result = result + temp
-		i = i + 1
+		i++
 	}
 
 	return result
 }
+
+// 2022/08/15 use stack
+// func romanToInt(s string) int {
+// 	stack := []rune{}
+// 	var result int
+// 	for _, c := range s {
+// 		switch c {
+// 		case 'M':
+// 			if len(stack) > 0 && stack[len(stack)-1] == 'C' {
+// 				result += 900
+// 				stack = stack[:len(stack)-1]
+// 			} else {
+// 				result += 1000
+// 			}
+// 		case 'D':
+// 			if len(stack) > 0 && stack[len(stack)-1] == 'C' {
+// 				result += 400
+// 				stack = stack[:len(stack)-1]
+// 			} else {
+// 				result += 500
+// 			}
+// 		case 'C':
+// 			if len(stack) > 0 && stack[len(stack)-1] == 'X' {
+// 				result += 90
+// 				stack = stack[:len(stack)-1]
+// 			} else {
+// 				stack = append(stack, c)
+// 			}
+// 		case 'L':
+// 			if len(stack) > 0 && stack[len(stack)-1] == 'X' {
+// 				result += 40
+// 				stack = stack[:len(stack)-1]
+// 			} else {
+// 				result += 50
+// 			}
+// 		case 'X':
+// 			if len(stack) > 0 && stack[len(stack)-1] == 'I' {
+// 				result += 9
+// 				stack = stack[:len(stack)-1]
+// 			} else {
+// 				stack = append(stack, c)
+// 			}
+// 		case 'V':
+// 			if len(stack) > 0 && stack[len(stack)-1] == 'I' {
+// 				result += 4
+// 				stack = stack[:len(stack)-1]
+// 			} else {
+// 				result += 5
+// 			}
+// 		default:
+// 			stack = append(stack, c)
+// 		}
+// 	}
+
+// 	for len(stack) > 0 {
+// 		switch c := stack[len(stack)-1]; c {
+// 		case 'M':
+// 			result += 1000
+// 		case 'D':
+// 			result += 500
+// 		case 'C':
+// 			result += 100
+// 		case 'L':
+// 			result += 50
+// 		case 'X':
+// 			result += 10
+// 		case 'V':
+// 			result += 5
+// 		default:
+// 			result += 1
+// 		}
+// 		stack = stack[:len(stack)-1]
+// 	}
+
+// 	return result
+// }

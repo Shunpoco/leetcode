d0f6b4cc8f4513aed5f192838fd4ea2383360035
Shunpoco <tkngsnsk313320@gmail.com>
2022-09-03 11:49:17 +0900

    numbers with same concecutive differences again

--- numbers-with-same-concecutive-differences/main.go
+++ numbers-with-same-concecutive-differences/main.go
@@ -1,29 +1,38 @@
 package main
 
 func numsSameConsecDiff(n int, k int) []int {
-    result := []int{}
-    
-    for i := 1; i <= 9; i++ {
-        solve(i, n-1, k, &result)
-    }
-    
-    return result
+	result := []int{}
+	memo := make(map[int][]int)
+	for i := 1; i <= 9; i++ {
+		solve(i, n-1, k, &result, &memo)
+	}
+
+	return result
 }
 
-func solve(digit, n, k int, result *[]int) {
-    if n == 0 {
-        (*result) = append(*result, digit)
-        return
-    }
-    
-    v := digit % 10
-    for i := 0; i <= 9; i++ {
-        diff := v - i
-        if diff < 0 {
-            diff *= -1
-        }
-        if diff == k {
-            solve(digit*10+i, n-1, k, result)
-        }
-    }
+func solve(digit, n, k int, result *[]int, memo *map[int][]int) {
+	if n == 0 {
+		(*result) = append(*result, digit)
+		return
+	}
+
+	v := digit % 10
+	if list, ok := (*memo)[v]; ok {
+		for _, i := range list {
+			solve(digit*10+i, n-1, k, result, memo)
+		}
+	} else {
+		l := []int{}
+		for i := 0; i <= 9; i++ {
+			diff := v - i
+			if diff < 0 {
+				diff *= -1
+			}
+			if diff == k {
+				l = append(l, i)
+				solve(digit*10+i, n-1, k, result, memo)
+			}
+		}
+		(*memo)[v] = l
+	}
 }

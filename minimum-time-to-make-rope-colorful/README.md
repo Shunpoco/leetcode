407df3904ae5b6ae5feb8d31c2c92ffd3818fa39
Shunpoco <tkngsnsk313320@gmail.com>
2022-10-03 13:18:57 +0900

    minimum time to make rope colorful on Python

---
+++ minimum-time-to-make-rope-colorful/README.md
@@ -0,0 +1,2 @@
+## Minimum time to make rope colorful
+https://leetcode.com/problems/minimum-time-to-make-rope-colorful/
---
+++ minimum-time-to-make-rope-colorful/main.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def minCost(self, colors: str, neededTime: List[int]) -> int:
+        cur = 0
+        prev = -1
+        result = 0
+        while cur < len(colors):
+            if prev >= 0 and colors[prev] == colors[cur]:
+                if neededTime[prev] <= neededTime[cur]:
+                    result += neededTime[prev]
+                    prev = cur
+                else:
+                    result += neededTime[cur]
+                                
+            else:
+                prev = cur
+            
+            cur += 1
+            
+        return result

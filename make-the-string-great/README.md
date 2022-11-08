ae366e1cf6d5af22128b0d44ff43dd7291d1b671
Shunpoco <tkngsnsk313320@gmail.com>
2022-11-08 13:53:50 +0900

    make the string great on Python

---
+++ make-the-string-great/README.md
@@ -0,0 +1,2 @@
+## Make the string great
+https://leetcode.com/problems/make-the-string-great/
---
+++ make-the-string-great/main.py
@@ -0,0 +1,11 @@
+class Solution:
+    def makeGood(self, s: str) -> str:
+        i = 0
+        while i < len(s)-1:
+            if (s[i].upper() == s[i+1] or s[i+1].upper() == s[i]) and s[i] != s[i+1]:
+                s = s[:i] + s[i+2:]
+                i = i - 1 if i > 0 else 0 
+            else:
+                i += 1
+            
+        return s

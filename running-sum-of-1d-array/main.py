fd2be8d159bed84e2cadbf35e7901a31b10204ff
Shunpoco <tkngsnsk313320@gmail.com>
2022-12-15 23:24:38 +0900

    running sum of 1d array on Python again

--- running-sum-of-1d-array/main.py
+++ running-sum-of-1d-array/main.py
@@ -2,11 +2,10 @@ from typing import List
 
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
-        cumulatives = []
+        cumulatives = [0 for _ in range(len(nums))]
         t = 0
-        for num in nums:
+        for i, num in enumerate(nums):
             t += num
-            cumulatives.append(t)
+            cumulatives[i] = t
 
         return cumulatives
-

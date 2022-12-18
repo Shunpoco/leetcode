ce34cfd82343e8e4f0295d435bfd8c5de44b4a6a
Shunpoco <tkngsnsk313320@gmail.com>
2022-12-18 11:40:37 +0900

    daily temperatures on Python faster solution

--- daily-temperatures/main.py
+++ daily-temperatures/main.py
@@ -1,17 +1,16 @@
 from typing import List
 
+# https://leetcode.com/problems/daily-temperatures/solutions/109818/simple-python-solution-w-explanation/
+
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        stack = []
-        result = []
+        memo = [-1 for _ in range(100-30+1)]
+        result = [0 for _ in range(len(temperatures))]
+
         for idx, temperature in reversed(list(enumerate(temperatures))):
-            while len(stack) > 0 and stack[-1][0] <= temperature:
-                stack.pop()
-            
-            ans = 0 if len(stack) == 0 else stack[-1][1]-idx
-            stack.append((temperature, idx))
+            t = [x for x in memo[temperature-30+1:] if x > -1]
+            r = 0 if len(t) == 0 else min(t) - idx
+            result[idx] = r
+            memo[temperature-30] = idx
 
-            result.insert(0, ans)
-            
         return result
-

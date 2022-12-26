1faa244a971a6ea1e052bc2fc497ed78f9bd8b17
Shunpoco <tkngsnsk313320@gmail.com>
2022-12-26 14:01:41 +0900

    jump game on Python with more sophisticated solution

--- jump-game/main.py
+++ jump-game/main.py
@@ -2,19 +2,11 @@ from typing import List
 
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        visited = [False for _ in range(len(nums))]
-        queue = [0]
-        visited[0] = True
+        n = len(nums)
+        goal = n-1
 
-        while len(queue) > 0:
-            v = queue.pop(0)
-            num = nums[v]
-            if v == len(nums)-1 or v + num >= len(nums)-1:
-                return True
+        for i in range(n-2, -1, -1):
+            if nums[i] >= goal - i:
+                goal = i
 
-            for i in range(1, num+1):
-                if not visited[v+i]:
-                    visited[v+i] = True
-                    queue.append(v+i)
-
-        return False
+        return goal == 0

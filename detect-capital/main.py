43103dad74e8944d76ed76bc2b2ce12421e881e4
Shunpoco <tkngsnsk313320@gmail.com>
2023-01-02 09:57:10 +0900

    detect capital on Python again

--- detect-capital/main.py
+++ detect-capital/main.py
@@ -3,12 +3,9 @@ class Solution:
         count_capital = 0
 
         for i, c in enumerate(word):
-            if count_capital == 0 and ord(c) < 97:
-                if i != 0:
+            if ord(c) < 97:
+                if count_capital == 0 and i != 0:
                     return False
-                count_capital += 1
-            elif ord(c) < 97:
                 count_capital += 1
 
         return count_capital <= 1 or count_capital == len(word)
-

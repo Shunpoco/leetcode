3a4560ca9b3052349c6951317677995f5e795500
Shunpoco <tkngsnsk313320@gmail.com>
2023-01-21 14:18:38 +0900

    restore ip addresses on Python again

--- restore-ip-addresses/main.py
+++ restore-ip-addresses/main.py
@@ -1,53 +1,38 @@
+from typing import List
+
 class Solution:
     def restoreIpAddresses(self, s: str) -> List[str]:
-        candidates = len(s)-1
-        stack = [[]]
         result = []
-        while len(stack) > 0:
-            v = stack.pop()
-            
-            if len(v) == 3:
-                r, b = self.valid(s, v)
-                if b:
-                    result.append(r)
-            
-            elif len(v) == 0:
-                for i in range(candidates):
-                    t = v.copy()
-                    t.append(i)
-                    stack.append(t)
-            else:
-                for i in range(v[-1]+1, candidates):
-                    t = v.copy()
-                    t.append(i)
-                    
-                    if len(t) == 3 or i != candidates-1:
-                        stack.append(t)
-        
+
+        self.execute(0, s, "", 0, result)
+
         return result
-    
-    def valid(self, s: str, nums: List[int]) -> bool:
-        r = ""
-        s_i = 1
-        n_i = 0
-        t = s[0]
-        
-        while n_i < 3:
-            if s_i > nums[n_i]:
-                if len(t) > 3 or (len(t) != 1 and t[0] == "0") or int(t) > 255:
-                    return ("", False)
-                r += t + "."
-                n_i += 1
-                t = ""
-            else:
-                t += s[s_i]
-                s_i += 1
-                
-        if s_i < len(s):
-            v = s[s_i:]
-            r += v
-            if len(v) > 3 or (len(v) != 1 and v[0] == "0") or int(v) > 255:
-                return ("", False)
-        
-        print(r)
-        return (r, True)
+
+
+    def execute(self, idx: int, s: str, ip: str, block: int, result: List[str]):
+        if idx == len(s) and block == 4:
+            result.append(ip[:len(ip)-1])
+            return
+        elif block >= 4:
+            return
+
+        for i in range(1, 4):
+            v = s[idx:idx+i]
+            if self.isValid(v):
+                ip += v + '.'
+                self.execute(idx+i, s, ip, block+1, result)
+                ip = ip[:len(ip)-(len(v)+1)]
+
+
+
+    def isValid(self, s: str) -> bool:
+        if len(s) == 0 or len(s) > 3:
+            return False
+
+        if len(s) > 1 and s[0] == '0':
+            return False
+
+        if int(s) > 255:
+            return False
+
+        return True

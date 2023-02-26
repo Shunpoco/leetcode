7dd71384f85ba9ce0e8f62485fc58698821b98a9
Shunpoco <tkngsnsk313320@gmail.com>
2023-02-26 11:25:13 +0000

    edit distance on Python

--- edit-distance/main.py
+++ edit-distance/main.py
@@ -1,20 +1,22 @@
-
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         n = len(word1)
         m = len(word2)
-        dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
-        
-        for r in range(n+1):
-            for c in range(m+1):
-                if r == 0:
-                    dp[r][c] = c
-                elif c == 0:
-                    dp[r][c] = r
+        dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
+
+        for i in range(m+1):
+            for j in range(n+1):
+                if i == 0 and j == 0:
+                    continue
+                if i == 0:
+                    dp[i][j] = dp[i][j-1] + 1
+                elif j == 0:
+                    dp[i][j] = dp[i-1][j] + 1
                 else:
-                    v = 1
-                    if word1[r-1] == word2[c-1]:
-                        v = 0
-                    dp[r][c] = min([dp[r-1][c]+1, dp[r][c-1]+1, dp[r-1][c-1]+v])
-            
-        return dp[n][m]
+                    if word1[j-1] == word2[i-1]:
+                        dp[i][j] = min(dp[i-1][j-1], dp[i-1][j]+1, dp[i][j-1]+1)
+                    else:
+                        dp[i][j] = min(dp[i-1][j-1]+1, dp[i-1][j]+1, dp[i][j-1]+1)
+
+        return dp[m][n]
+

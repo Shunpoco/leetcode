7a3b78adc6b0c7d8f12df2824401ffd748b22558
Shunpoco <tkngsnsk313320@gmail.com>
2023-04-10 23:07:25 +0100

    valid parenthesis on Python again

--- valid-parentheses/valid-parentheses.py
+++ valid-parentheses/valid-parentheses.py
@@ -1,65 +1,21 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        if len(s) % 2 != 0:
-            return False
-
-        d = {
+        pairs = {
+            '}': '{',
             ')': '(',
             ']': '[',
-            '}': '{',
         }
-        
+
         stack = []
-        
-        stack.append(s[0])
 
-        for i in range(1, len(s)):
-            c = s[i]
-            if c not in d:
-                stack.append(c)
-                continue
-
-            if not stack:
-                return False
-            
-            if d[c] == stack[-1]:
-                stack.pop(-1)
-                
-            else:
-                return False
-    
-        if stack:
-            return False
-        
-        return True
-
-
-# 2021-07-28
-import queue
-
-class Solution:
-    def isValid(self, s: str) -> bool:
-        combs = {
-            ')': '(',
-            ']': '[',
-            '}': '{',
-        }
-        
-        stack = queue.LifoQueue() # LIFOQueue = Stack
-        
         for c in s:
-            if c in combs.keys():
-                if stack.empty():
+            if pairs.get(c) is not None:
+                if len(stack) > 0 and stack[-1] == pairs[c]:
+                    stack.pop()
+
+                else:
                     return False
-                v = stack.get()
-                if combs[c] != v:
-                    return False
-                
             else:
-                stack.put(c)
-                
-                
-        if not stack.empty():
-            return False
-        
-        return True
+                stack.append(c)
+
+        return len(stack) == 0

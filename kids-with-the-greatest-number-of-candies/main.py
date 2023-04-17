07770a1bbb6710f2014465dc4be3748f98d08f76
Shunpoco <tkngsnsk313320@gmail.com>
2023-04-17 20:17:33 +0100

    kids with the greatest number of candies on Python again

--- kids-with-the-greatest-number-of-candies/main.py
+++ kids-with-the-greatest-number-of-candies/main.py
@@ -3,12 +3,5 @@ from typing import List
 class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
         m = max(candies)
-
-        result = [False for _ in range(len(candies))]
-
-        for i, v in enumerate(candies):
-            if v + extraCandies >= m:
-                result[i] = True
-
-        return result
+        return [True if v + extraCandies >= m else False for v in candies]
 

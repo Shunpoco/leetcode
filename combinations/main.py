dbffee95fdc0278a4214a2356c82088d6f48e4a8
Shunpoco <tkngsnsk313320@gmail.com>
2023-08-01 13:55:51 +0100

    combinations on Python

--- combinations/main.py
+++ combinations/main.py
@@ -1,27 +1,24 @@
+from typing import List
+
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        stack = [[]]
-        
-        nums = list(range(1, n+1))
         result = []
-        
-        while len(stack) > 0:
-            vals = stack.pop(-1)
-            
-            if len(vals) > 0:
-                nums_temp = nums[vals[-1]:]
-            else:
-                nums_temp = nums
 
-            for num in nums_temp:
-                if num not in vals:
-                    temp = vals.copy()
-                    temp.append(num)
-                    
-                    if len(temp) == k:
-                        result.append(temp)
-                    else:
-                        stack.append(temp)
-                        
+        def choose(cur: List[int]):
+            if len(cur) == k:
+                result.append(cur)
+                return
+
+            c = cur[-1] if len(cur) > 0 else 0
+
+            if len(cur) + (n-c) < k:
+                return
+
+            for i in range(c+1, n+1):
+                t = cur.copy()
+                t.append(i)
+                choose(t)
+
+        choose([])
+
         return result
-

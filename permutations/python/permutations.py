e4895eb9254fdc7bfc16dad967dc510647116beb
Shunpoco <tkngsnsk313320@gmail.com>
2023-08-02 17:38:35 +0100

    permutations on Python

--- permutations/python/permutations.py
+++ permutations/python/permutations.py
@@ -1,52 +1,25 @@
 from typing import List
 
-# class Solution:
-#     def permute(self, nums: List[int]) -> List[List[int]]:
-#         stack = [([], 0)]
-#         result = []
-#         L = len(nums)
-        
-#         while stack:
-#             p, l = stack.pop(-1)
-            
-#             for num in nums:
-#                 if num not in p:
-#                     t = p.copy()
-#                     t.append(num)
-                    
-#                     if len(t) == L:
-#                         result.append(t)
-#                     else:
-#                         stack.append([t, l+1])
-                        
-#         return result
-
-
-# Using backtrack
-from collections import Counter
-
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        c = Counter()
-        
-        for num in nums:
-            c[num] += 1
-            
+        n = len(nums)
+
+        visited = [False for _ in range(n)]
         result = []
+        v = []
+        def calc():
+            if len(v) == n:
+                result.append(v.copy())
+                return
 
-        self.backtrack(nums, [], result, c)
-        
+            for i, num in enumerate(nums):
+                if visited[i] == False:
+                    visited[i] = True
+                    v.append(num)
+                    calc()
+                    v.pop(-1)
+                    visited[i] = False
+
+        calc()
+
         return result
-
-    def backtrack(self, nums: List[int], v: List[int], result: List[List[int]], c: Counter):
-        if len(v) == len(nums):
-            result.append(v.copy())
-            return
-        
-        for num in nums:
-            if c[num] -1 >= 0:
-                v.append(num)
-                c[num] -= 1
-                self.backtrack(nums, v, result, c)
-                c[num] += 1
-                v.pop(-1)

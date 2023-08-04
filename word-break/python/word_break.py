78db6393a1a9a756c30173246a58eb6741baeb4d
Shunpoco <tkngsnsk313320@gmail.com>
2023-08-04 23:42:34 +0100

    word break on Python

--- word-break/python/word_break.py
+++ word-break/python/word_break.py
@@ -1,6 +1,3 @@
-# https://leetcode.com/problems/word-break/discuss/43995/A-Simple-Python-DP-solution
-from typing import List
-
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         dp = [False] * (len(s) + 1) # dp[i] means s[:i+1] can be segmented into words in the wordDict

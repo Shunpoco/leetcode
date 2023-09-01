8f631369e08ef3c2d5a2d749bf57798ac465b01d
Shunpoco <tkngsnsk313320@gmail.com>
2023-09-01 22:04:35 +0100

    counting bits on Python

--- counting-bits/main.py
+++ counting-bits/main.py
@@ -2,14 +2,12 @@ from typing import List
 
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        result = []
-        
-        for i in range(0, n+1):
-            if i == 0 or i == 1:
-                result.append(i)
-            else:
-                if i % 2 == 0:
-                    result.append(result[int(i/2)])
-                else:
-                    result.append(result[i-1]+1)
+        result = [0]
+
+        nex = 1
+        for i in range(1, n+1):
+            result.append(1+result[i-nex])
+            if i == nex:
+                nex *= 2
+
         return result

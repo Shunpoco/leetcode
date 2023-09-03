283d6a0f7485531b35120672e92e2fe5cd8ac9b7
Shunpoco <tkngsnsk313320@gmail.com>
2023-09-03 14:55:50 +0100

    unique paths on Pythoun

--- unique-paths/main.py
+++ unique-paths/main.py
@@ -1,21 +1,9 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        memory = {}
-        
-        return self.paths(m, n, memory)                
-    
-    def paths(self, m: int, n: int, mem: Dict) -> int:
-        if mem.get((m, n)) is not None:
-            return mem.get((m, n))
-            
-        if m == 1 or n == 1:
-            v = 1
+        dp = [1 for _ in range(n)]
 
-        else:
-            v1 = self.paths(m-1, n, mem)
-            v2 = self.paths(m, n-1, mem)
-            v = v1 + v2
+        for _ in range(m-2, -1, -1):
+            for j in range(n-2, -1, -1):
+                    dp[j] += dp[j+1]
 
-        mem[(m, n)] = v
-        
-        return v
+        return dp[0]

5305c24eac7c15efcd0c1eb2871cf4288ab2d7d5
Shunpoco <tkngsnsk313320@gmail.com>
2023-12-26 09:46:34 +0000

    number of dice rolls with target sum on Python

--- number-of-dice-rolls-with-target-sum/main.py
+++ number-of-dice-rolls-with-target-sum/main.py
@@ -1,26 +1,33 @@
 class Solution:
     def numRollsToTarget(self, n: int, k: int, target: int) -> int:
-        mod = 1000000007
-        dp = [[0 for _ in range(target+1)] for _ in range(n)]
-        
-        for i in range(1, k+1):
-            if i > target:
-                break
-            dp[0][i] = 1
-            
-        for i in range(1, n):
-            s = 0
-            right = 0
-            left = right - k
-            for j in range(1, target+1):
-                s += dp[i-1][right]
-                s %= mod
-                if left >= 0:
-                    s -= dp[i-1][left]
-                    s %= mod
-                right += 1
-                left += 1
+        mod = 10 ** 9 + 7
 
-                dp[i][j] = s
-                
-        return dp[n-1][target]
+        memo = {}
+
+        def solve(n, target):
+            if memo.get((n, target)) is not None:
+                return
+
+            if n == 0 and target == 0:
+                memo[(n, target)] = 1
+                return
+
+            if target < -1:
+                memo[(n, target)] = 0
+                return
+            
+            if n == 0:
+                memo[(n, target)] = 0
+                return
+
+            result = 0
+            for i in range(1, k+1):
+                solve(n-1, target-i)
+                result += memo[(n-1, target-i)]
+                result %= mod
+
+            memo[(n, target)] = result
+
+        solve(n, target)
+
+        return memo[(n, target)]

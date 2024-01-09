3933b172108e7960f400d583e9f4d39b6aa6a9cf
Shunpoco <tkngsnsk313320@gmail.com>
2024-01-09 18:34:13 +0000

    leaf similar trees on Python again

--- leaf-similar-trees/main.py
+++ leaf-similar-trees/main.py
@@ -6,24 +6,21 @@
 #         self.right = right
 class Solution:
     def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
-        v1 = self.getSequence(root1)
-        v2 = self.getSequence(root2)
+        r1 = []
+        r2 = []
 
-        return v1 == v2
+        self.leafs(root1, r1)
+        self.leafs(root2, r2)
 
-    def getSequence(self, root: Optional[TreeNode]) -> List[int]:
-        stack = [root]
-        result = []
+        return r1 == r2
 
-        while len(stack) > 0:
-            node = stack.pop(-1)
-            if node.left is None and node.right is None:
-                result.append(node.val)
+    def leafs(self, node, result):
+        if node.left is None and node.right is None:
+            result.append(node.val)
 
-            if node.right is not None:
-                stack.append(node.right)
+        if node.left is not None:
+            self.leafs(node.left, result)
 
-            if node.left is not None:
-                stack.append(node.left)
+        if node.right is not None:
+            self.leafs(node.right, result)
 
-        return result

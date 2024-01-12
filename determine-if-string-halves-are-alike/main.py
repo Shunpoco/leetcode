545a42d17c30c5d170c542ec7d5e2a1252d4347b
Shunpoco <tkngsnsk313320@gmail.com>
2024-01-12 22:52:34 +0000

    determine if string halves are alike on Python

--- determine-if-string-halves-are-alike/main.py
+++ determine-if-string-halves-are-alike/main.py
@@ -1,17 +1,13 @@
 class Solution:
     def halvesAreAlike(self, s: str) -> bool:
-        vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
-        
-        l = len(s)
-        
-        c1 = 0
-        for i in range(0, l//2):
-            if s[i] in vowels:
-                c1 +=1
-                
-        c2 = 0
-        for i in range(l//2, l):
-            if s[i] in vowels:
-                c2 += 1
-                
-        return c1 == c2
+        c = 0
+
+        for i in range(len(s) // 2):
+            if s[i] in ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']:
+                c += 1
+
+        for i in range(len(s) // 2, len(s)):
+            if s[i] in ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']:
+                c -= 1
+
+        return c == 0

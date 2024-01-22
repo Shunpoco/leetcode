23e42fc7727728c798d6a7d941277c7945a175cb
Shunpoco <tkngsnsk313320@gmail.com>
2024-01-22 20:15:51 +0000

    set mismatch on Python

--- set-mismatch/main.py
+++ set-mismatch/main.py
@@ -1,19 +1,15 @@
-from typing import List
-
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        r = [0]*n
-        
+
+        memo = [0 for _ in range(n)]
+
         dup = -1
         for num in nums:
-            r[num-1] += 1
-            if r[num-1] == 2:
+            memo[num-1] += 1
+            if memo[num-1] == 2:
                 dup = num
-                
-        no = -1
-        for i, v in enumerate(r):
-            if v == 0:
-               no = i+1
-            
+
+        no = memo.index(0) + 1
+
         return [dup, no]

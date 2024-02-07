02e8f7ebee2f2a7080303fddea8919c3fdbf5ab0
Shunpoco <tkngsnsk313320@gmail.com>
2024-02-07 22:22:01 +0000

    sort characters by frequency on Python

--- sort-characters-by-frequency/main.py
+++ sort-characters-by-frequency/main.py
@@ -1,11 +1,17 @@
-from collections import Counter
-
 class Solution:
     def frequencySort(self, s: str) -> str:
-        counter = Counter(s)
-        
+        memo = defaultdict(int)
+
+        for c in s:
+            memo[c] += 1
+
+        v = [(v, k) for k, v in memo.items()]
+
+        v.sort()
+
         r = []
-        for c, n in counter.most_common():
-            r.append(c*n)
-            
+
+        for i in range(len(v)-1, -1, -1):
+            r.append(v[i][1]*v[i][0])
+
         return "".join(r)

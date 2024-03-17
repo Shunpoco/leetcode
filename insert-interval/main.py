874b6f02a0cf4b6b20958564838e95c8ca5e2ec3
Shunpoco <tkngsnsk313320@gmail.com>
2024-03-17 23:36:04 +0000

    insert interval

--- insert-interval/main.py
+++ insert-interval/main.py
@@ -1,49 +1,23 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        intervals = self._insert(intervals, newInterval)
-        print(intervals)
-        return self.merge(intervals)
-    
-    
-    
-    def _insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        if not intervals:
-            return [newInterval]
-        if len(intervals) == 1:
-            if intervals[0] < newInterval:
-                return [intervals[0], newInterval]
-            else:
-                return [newInterval, intervals[0]]
-            
-        m = int(len(intervals)/2)
-        
-        l = intervals[:m]
-        r = intervals[m:]
-        
-        if intervals[m] > newInterval:
-            l = self._insert(l, newInterval)
-        else:
-            r = self._insert(r, newInterval)
-            
-        l.extend(r)
-        
-        return l
-    
-    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        # intervals are already sorted
-        
-        res = [intervals.pop(0)]
-        i = 0
-        
-        while intervals:
-            s = res[i]
-            l = intervals.pop(0)
-            if s[1] >= l[0]:
-                # overlapping
-                res[i] = [s[0], max(s[1], l[1])]
-                
-            else:
-                res.append(l)
-                i += 1
-                
-        return res
+        n = len(intervals)
+        result = []
+
+        idx = 0
+
+        while idx < n and intervals[idx][1] < newInterval[0]:
+            result.append(intervals[idx])
+            idx += 1
+
+        while idx < n and intervals[idx][0] <= newInterval[1]:
+            newInterval[0] = min(intervals[idx][0], newInterval[0])
+            newInterval[1] = max(intervals[idx][1], newInterval[1])
+            idx += 1
+
+        result.append(newInterval)
+
+        while idx < n:
+            result.append(intervals[idx])
+            idx += 1
+
+        return result

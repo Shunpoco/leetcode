264b7520a204ff1feb157c0c294532ee3174df01
Shunpoco <tkngsnsk313320@gmail.com>
2024-04-01 19:36:38 +0100

    length of last word on Python

--- length-of-last-word/length_of_last_word.py
+++ length-of-last-word/length_of_last_word.py
@@ -1,11 +1,13 @@
 class Solution:
-    def lengthOfLastWord(self, s: str)-> int:
-        c = s
-        # prepare
-        while len(c) > 0:
-            if c[-1] == ' ':
-                c = c[:len(c)-1]
-            else:
-                break
-        a = c.split(' ')
-        return len(a[-1])
+    def lengthOfLastWord(self, s: str) -> int:
+        n = len(s)
+
+        c = 0
+        for i in range(n-1, -1, -1):
+            if s[i] != ' ':
+                c += 1
+            elif c != 0:
+                return c
+
+        return c
+

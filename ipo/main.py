95f944a81bdda4a6a5f0e0a711b27b1f67bf7ac5
Shunpoco <tkngsnsk313320@gmail.com>
2024-06-15 14:48:41 +0100

    ipo on Python again

--- ipo/main.py
+++ ipo/main.py
@@ -1,26 +1,23 @@
-from typing import List
-
 import heapq
 
 class Solution:
     def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
-        tasks = [(c, -p) for c, p in zip(capital, profits)]
-        tasks.sort()
+        projects = [(c, p) for c, p in zip(capital, profits)]
+        projects.sort()
 
-        heap = []
-        for _ in range(k):
-            if len(tasks) > 0:
-                v = tasks[0]
-                while v[0] <= w:
-                    heapq.heappush(heap, v[1])
-                    tasks.pop(0)
-                    if len(tasks) == 0:
-                        break
-                    v = tasks[0]
+        hq = []
 
-            if len(heap) > 0:
-                w += -1*heapq.heappop(heap)
+        i = 0
+        while k > 0:
+            while i < len(projects) and projects[i][0] <= w:
+                heapq.heappush(hq, -projects[i][1])
+                i += 1
 
+            if len(hq) == 0:
+                break
+            p = heapq.heappop(hq)
+            w += -p
+            k -= 1
 
         return w
 

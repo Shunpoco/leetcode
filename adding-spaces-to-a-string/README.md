46df807cf4ae85131403f65903f74400a2c38196
Shunpoco <tkngsnsk313320@gmail.com>
2024-12-03 21:30:08 +0000

    adding spaces to a string on Python

---
+++ adding-spaces-to-a-string/README.md
@@ -0,0 +1,2 @@
+## Adding spaces to a string
+https://leetcode.com/problems/adding-spaces-to-a-string/
---
+++ adding-spaces-to-a-string/main.py
@@ -0,0 +1,13 @@
+class Solution:
+    def addSpaces(self, s: str, spaces: List[int]) -> str:
+        result = ""
+
+        prev = 0
+        for space in spaces:
+            result += s[prev:space] + " "
+            prev = space
+
+        result += s[prev:]
+
+        return result
+

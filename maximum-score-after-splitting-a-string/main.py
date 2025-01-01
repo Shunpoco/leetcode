bc5456c0508d565b1a5413d2c610819bb6bef734
Shunpoco <tkngsnsk313320@gmail.com>
2025-01-01 12:56:11 +0000

    maximum score after splitting a string on Python

--- maximum-score-after-splitting-a-string/main.py
+++ maximum-score-after-splitting-a-string/main.py
@@ -1,24 +1,26 @@
 class Solution:
     def maxScore(self, s: str) -> int:
-        zeros = [0 for _ in range(len(s))]
-        ones = [0 for _ in range(len(s))]
+        c_zero = [0 for _ in range(len(s))]
+        c_one = [0 for _ in range(len(s))]
 
-        for i, c in enumerate(s):
-            if i != 0:
-                zeros[i] = zeros[i-1]
-            if c == '0':
-                zeros[i] += 1
+        t = 0
+        for i in range(len(s)):
+            if s[i] == '0':
+                t += 1
+            c_zero[i] = t
 
+        t = 0
         for i in range(len(s)-1, -1, -1):
-            if i != len(s)-1:
-                ones[i] = ones[i+1]
             if s[i] == '1':
-                ones[i] += 1
+                t += 1
+            c_one[i] = t
 
         result = 0
         for i in range(len(s)-1):
-            t = zeros[i] + ones[i+1]
+            t = c_zero[i] + c_one[i+1]
+
             if t > result:
                 result = t
 
         return result
+

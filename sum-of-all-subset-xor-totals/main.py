9bb54f922b92d42ca1d500712011262f4af65617
Shunpoco <tkngsnsk313320@gmail.com>
2025-04-05 23:54:43 +0200

    sum of all subset xor totals on Python

--- sum-of-all-subset-xor-totals/main.py
+++ sum-of-all-subset-xor-totals/main.py
@@ -1,15 +1,12 @@
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
-        n = len(nums)
-
         r = 0
-        for i in range(2**n):
+        for i in range(2**len(nums)):
             t = 0
-            for j in range(n):
+            for j in range(len(nums)):
                 if i & 2**j > 0:
                     t ^= nums[j]
 
             r += t
 
         return r
-

c47e6ed8d6190508107164bb929b606692975eab
Shunpoco <tkngsnsk313320@gmail.com>
2025-04-26 13:35:37 +0100

    longest subsequence with limited sum on Python again

--- longest-subsequence-with-limited-sum/main.py
+++ longest-subsequence-with-limited-sum/main.py
@@ -1,25 +1,18 @@
 class Solution:
     def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
-        n = len(nums)
-        m = len(queries)
-        answer = [-1 for _ in range(m)]
-
+        nums.sort()
         sums = sum(nums)
 
-        pq = []
-        for num in nums:
-            heapq.heappush(pq, -num)
-
-        print(pq)
+        answer = [-1 for _ in range(len(queries))]
 
         for i, q in enumerate(queries):
             t = sums
-            pq_ = pq.copy()
+            cur = len(nums)-1
 
-            while t > q:
-                v = heapq.heappop(pq_)
-                t -= -v
+            while t > q and cur >= 0:
+                t -= nums[cur]
+                cur -= 1
 
-            answer[i] = len(pq_)
+            answer[i] = cur + 1
 
         return answer

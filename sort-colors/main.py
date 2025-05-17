03d829dc8e3fffaa6ff84032c6fbbdf5c8540ec8
Shunpoco <tkngsnsk313320@gmail.com>
2025-05-17 23:25:51 +0100

    sort colors

--- sort-colors/main.py
+++ sort-colors/main.py
@@ -3,16 +3,15 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        memo = {}
-        memo[0] = 0
-        memo[1] = 0
-        memo[2] = 0
+        memo = [0] * 3
+
         for num in nums:
             memo[num] += 1
 
-        i = 0
-        for j in range(3):
-            for _ in range(memo[j]):
-                nums[i] = j
-                i += 1
+        idx = 0
+        for i in range(3):
+            while memo[i] > 0:
+                nums[idx] = i
+                memo[i] -= 1
+                idx += 1
 

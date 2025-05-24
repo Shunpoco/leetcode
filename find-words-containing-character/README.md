a2d8914a533a3d975e439fe13554f5b0bc7e187c
Shunpoco <tkngsnsk313320@gmail.com>
2025-05-24 21:36:55 +0100

    find words containing character on Python

---
+++ find-words-containing-character/README.md
@@ -0,0 +1,2 @@
+## Find words containing character
+https://leetcode.com/problems/find-words-containing-character
---
+++ find-words-containing-character/main.py
@@ -0,0 +1,9 @@
+impl Solution {
+    pub fn find_words_containing(words: Vec<String>, x: char) -> Vec<i32> {
+        let mut result = Vec::new();
+
+        for (i, word) in words.enumerate() {
+            if 
+        }
+    }
+}

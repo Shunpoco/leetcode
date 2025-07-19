3312c098f73ef5afcdeda2b25e52dd32cb87133b
Shunpoco <tkngsnsk313320@gmail.com>
2025-07-19 18:12:34 +0100

    remove sub folders from the filesystem on Python again

--- remove-sub-folders-from-the-filesystem/main.py
+++ remove-sub-folders-from-the-filesystem/main.py
@@ -1,3 +1,5 @@
+from typing import List
+
 class Solution:
     def removeSubfolders(self, folder: List[str]) -> List[str]:
         folder.sort()
@@ -15,6 +17,27 @@ class Solution:
         result.append(v)
 
         return result
-    
+
     def isSub(self, a, b):
         return b[:len(a)] == a and (len(a) == len(b) or b[len(a)] == "/")
+
+def test1():
+    folder = ["/a", "/a/b", "/c/d", "/c/d/e", "/c/f"]
+    result  = Solution().removeSubfolders(folder)
+    expected = ["/a", "/c/d", "/c/f"]
+
+    assert result == expected
+
+def test2():
+    folder = ["/a", "/a/b/c", "/a/b/d"]
+    result = Solution().removeSubfolders(folder)
+    expected = ["/a"]
+
+    assert result == expected
+
+def main():
+    test1()
+    test2()
+
+if __name__ == '__main__':
+    main()

e69aa9ba1ff8c431269811dca8f9785ac43b79ab
Shunpoco <tkngsnsk313320@gmail.com>
2025-08-16 09:20:33 +0100

    maximum 69 number

--- maximum-69-number/main.py
+++ maximum-69-number/main.py
@@ -1,15 +1,15 @@
 class Solution:
     def maximum69Number (self, num: int) -> int:
-        result = num
-        
-        digit = 1
         t = num
-        while digit < num:
-            v = t % 10
-            if v == 6:
-                result = num - 6*digit + 9*digit
-            
-            digit *= 10
-            t = t // 10
-            
-        return result
+        i = 1
+        p = 0
+
+        while t > 0:
+            if t % 10 == 6:
+                p = i
+
+            t //= 10
+            i *= 10
+
+        return  num - 6 * p + 9 * p
+

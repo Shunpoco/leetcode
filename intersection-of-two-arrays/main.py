b3b20abab39f3e1e04a1bc765ef64fe65816a5fa
Shunpoco <tkngsnsk313320@gmail.com>
2025-08-18 22:20:00 +0100

    intersection of two arrays on Python

--- intersection-of-two-arrays/main.py
+++ intersection-of-two-arrays/main.py
@@ -1,13 +1,16 @@
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        n = [0 for _ in range(1001)]
+        memo = {}
 
         for num in nums1:
-            n[num] = 1
+            memo[num] = True
 
-        res = {}
+        result = []
+
         for num in nums2:
-            if n[num] == 1:
-                res[num] = True
+            if memo.get(num, False):
+                result.append(num)
+                memo[num] = False
 
-        return list(res.keys())
+        return result
+

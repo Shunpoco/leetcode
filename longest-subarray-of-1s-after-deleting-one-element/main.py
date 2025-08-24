16aeef25111c703f4b0c36197e9d6cc9a7cbf7b4
Shunpoco <tkngsnsk313320@gmail.com>
2025-08-24 23:31:45 +0100

    longest subarray of 1s after deleting one element on Python again

--- longest-subarray-of-1s-after-deleting-one-element/main.py
+++ longest-subarray-of-1s-after-deleting-one-element/main.py
@@ -1,30 +1,28 @@
-from typing import List
-
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
-        ones = []
+        sums = []
 
         t = 0
         for num in nums:
-            if num == 1:
-                t += 1
-            else:
-                ones.append(t)
+            if num == 0:
+                if t > 0:
+                    sums.append(t)
+                sums.append(0)
                 t = 0
-        if t > 0:
-            ones.append(t)
+            else:
+                t += 1
 
-        n = len(ones)
+        sums.append(t)
+
+        if len(sums) == 1:
+            return max(0, sums[0]-1)
+
         result = 0
-        if n == 0:
-            return result
-        elif n == 1:
-            return ones[0]-1
-
-        for i in range(n-1):
-            v = ones[i] + ones[i+1]
-            if v > result:
-                result = v
-
+        for i in range(len(sums)):
+            if i > 0 and i < len(sums)-1 and sums[i] == 0:
+                result = max(result, sums[i-1] + sums[i+1])
+            else:
+                result = max(result, sums[i])
+        
         return result
 
